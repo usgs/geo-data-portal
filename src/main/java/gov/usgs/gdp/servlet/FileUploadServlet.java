dedca2c4861d47fbf060f70cf3a744d775487993
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2010-01-13 23:41:53 +0000

    refactoring - no functional changes
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@8547 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- src/main/java/gov/usgs/gdp/servlet/FileUploadServlet.java
+++ src/main/java/gov/usgs/gdp/servlet/FileUploadServlet.java
@@ -10,52 +10,62 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
 /**
  * Servlet implementation class FileUploadServlet
  */
 public class FileUploadServlet extends HttpServlet {
+	private static org.apache.log4j.Logger log = Logger
+			.getLogger(FileUploadServlet.class);
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public FileUploadServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 BufferedInputStream buf = null;
-		 ServletOutputStream stream = null;
-		try {
-			stream = response.getOutputStream();
-			String file 							= (request.getParameter("file") == null) ? "" : request.getParameter("file");
-			File sendFile = new File(file);
-			 response.addHeader("Content-Disposition", "attachment; filename="
-			          + file);
-			 response.setContentLength((int) sendFile.length());
-			 FileInputStream input = new FileInputStream(file);
-			 buf = new BufferedInputStream(input);
-		      int readBytes = 0;
-		      //read from the file; write to the ServletOutputStream
-		      while ((readBytes = buf.read()) != -1) stream.write(readBytes);
-		} catch (IOException ioe) {
-		      throw new ServletException(ioe.getMessage());
-		    } finally {
-		      if (stream != null)
-		        stream.close();
-		      if (buf != null)
-		        buf.close();
-		    }
+	public FileUploadServlet() {
+		super();
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		BufferedInputStream buf = null;
+		ServletOutputStream stream = null;
+		try {
+			stream = response.getOutputStream();
+			String file = (request.getParameter("file") == null) ? "" : request.getParameter("file");
+			File sendFile = new File(file);
+			response.addHeader("Content-Disposition", "attachment; filename=" + file);
+			response.setContentLength((int) sendFile.length());
+			FileInputStream input = new FileInputStream(file);
+			
+			// read from the file; write to the ServletOutputStream
+			int readBytes = 0;
+			buf = new BufferedInputStream(input);
+			while ((readBytes = buf.read()) != -1) stream.write(readBytes);
+		} catch (IOException ioe) {
+			log.debug(ioe.getMessage());
+			throw new ServletException(ioe.getMessage());
+		} finally {
+			if (stream != null)
+				stream.close();
+			if (buf != null)
+				buf.close();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
 
