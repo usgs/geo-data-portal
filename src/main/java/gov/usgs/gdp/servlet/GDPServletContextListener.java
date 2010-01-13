a46c78c2e2e5849f284a6a3a84909fbb9f06d326
tkunicki <tkunicki@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2010-01-13 19:06:00 +0000

    Some PermGen cleanup stuff
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@8536 622dc1fb-2224-0410-ab72-dc85a3975e1a

---
+++ src/main/java/gov/usgs/gdp/servlet/GDPServletContextListener.java
@@ -0,0 +1,45 @@
+package gov.usgs.gdp.servlet;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
+
+/**
+ * Application Lifecycle Listener implementation class GSPServletContextListener
+ * 
+ */
+public class GDPServletContextListener implements ServletContextListener {
+
+    /**
+     * Default constructor.
+     */
+    public GDPServletContextListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sre) {
+        // TODO Clean up temp files from previous application instance
+        // (unexpected JVM exit)
+    }
+
+    /**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sre) {
+        // TODO Clean up temp files from this application instance (unexpected
+        // JVM exit)
+
+        // Clean up after MultiThreadedHttpConnectionManager
+        // NOTE: Check class loaders to guarantee
+        // MultiThreadedHttpConnectionManager is associated with this
+        // Servlet Context's ClassLoader
+        if (MultiThreadedHttpConnectionManager.class.getClassLoader() == 
+                Thread.currentThread().getContextClassLoader()) {
+            MultiThreadedHttpConnectionManager.shutdownAll();
+        }
+    }
+}
