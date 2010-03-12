87f9d3ca1c7a588d5a9ad8020f23efc0f6e4632b
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2010-03-12 19:37:39 +0000

    XmlReplyBean has full test coverage
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@9294 622dc1fb-2224-0410-ab72-dc85a3975e1a

---
+++ src/test/java/gov/usgs/gdp/bean/XmlReplyBeanTest.java
@@ -0,0 +1,285 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gov.usgs.gdp.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author isuftin
+ */
+public class XmlReplyBeanTest {
+
+    XmlReplyBean instance = null;
+
+    public XmlReplyBeanTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+        this.instance = new XmlReplyBean();
+    }
+
+    @After
+    public void tearDown() {
+        this.instance = null;
+    }
+
+    @Test
+    public void testXmlReplyBeanConstructorAckBeanAndContentArray() {
+        XmlReplyBean result = new XmlReplyBean(new AckBean(AckBean.ACK_OK), new AckBean(AckBean.ACK_OK));
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testXmlReplyBeanConstructorWithInStatusAndContentArray() {
+        XmlReplyBean result = new XmlReplyBean(1, new AckBean(AckBean.ACK_OK));
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testXmlReplyBeanConstructorWithInStatusAndContentArrayWithIllegalArgumentException() {
+        try {
+            XmlReplyBean result = new XmlReplyBean(-1, new AckBean(AckBean.ACK_OK));
+            assertNotNull(result);
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+    }
+
+    @Test
+    public void testXmlReplyBeanConstructorWithInStatusAndMessageBean() {
+        XmlReplyBean result = new XmlReplyBean(1, new MessageBean());
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testXmlReplyBeanConstructorWithInStatusAndMessageBeanWithIllegalArgumentException() {
+
+        try {
+            XmlReplyBean result = new XmlReplyBean(-1, new MessageBean());
+            assertNotNull(result);
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+    }
+
+    @Test
+    public void testXmlReplyBeanConstructorWithInStatusAndErrorEnumAndMessageBeanWithIllegalArgumentException() {
+        try {
+            XmlReplyBean result = new XmlReplyBean(-1, ErrorEnum.ERR_ATTRIBUTES_NOT_FOUND, new MessageBean());
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+    }
+
+    @Test
+    public void testXmlReplyBeanConstructorWithIntAndXmlBeanList() {
+        List content = new ArrayList();
+        content.add(new AckBean(AckBean.ACK_OK));
+        XmlReplyBean result = new XmlReplyBean(1, content);
+        assertNotNull(result);
+        assertTrue(result.getContent().length > 0);
+    }
+
+    @Test
+    public void testXmlReplyBeanConstructorWithMessageBeanAndContentArray() {
+        XmlReplyBean result = new XmlReplyBean(new MessageBean("Test"), new XmlBean[]{null, null});
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testXmlReplyBeanConstructorWithStatusInt() {
+        XmlReplyBean result = new XmlReplyBean(1);
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testXmlReplyBeanConstructorWithStatusIntWithIllegalArgumentException() {
+        try {
+            XmlReplyBean result = new XmlReplyBean(-1);
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+    }
+
+    @Test
+    public void testXmlReplyBeanConstructorWithStatusIntAndErrorBean() {
+        XmlReplyBean result = new XmlReplyBean(1, new ErrorBean());
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testXmlReplyBeanConstructorWithStatusIntAndErrorBeanWithIllegalArgumentException() {
+        try {
+            XmlReplyBean result = new XmlReplyBean(-1, new ErrorBean());
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+    }
+
+    @Test
+    public void testXmlReplyBeanConstructorWithStatusIntAndErrorEnum() {
+        XmlReplyBean result = new XmlReplyBean(1, ErrorEnum.ERR_ATTRIBUTES_NOT_FOUND);
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testXmlReplyBeanConstructorWithStatusIntAndErrorEnumWithIllegalArgumentException() {
+        try {
+            XmlReplyBean result = new XmlReplyBean(-1, ErrorEnum.ERR_ATTRIBUTES_NOT_FOUND);
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+    }
+
+    @Test
+    public void testXmlReplyBeanConstructorWithStatusIntAndErrorEnumAndMessageBean() {
+        XmlReplyBean result = new XmlReplyBean(1, ErrorEnum.ERR_ATTRIBUTES_NOT_FOUND, new MessageBean());
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testXmlReplyBeanConstructorWithStatusIntAndErrorEnumAndMessageBeanWithIllegalArgumentException() {
+        try {
+            XmlReplyBean result = new XmlReplyBean(-1, ErrorEnum.ERR_ATTRIBUTES_NOT_FOUND, new MessageBean());
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+    }
+
+    @Test
+    public void testXmlReplyBeanConstructorWithStatusIntAndErrorBeanAndMessageBean() {
+        XmlReplyBean result = new XmlReplyBean(1, new ErrorBean(), new MessageBean());
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testXmlReplyBeanConstructorWithStatusIntAndErrorBeanAndMessageBeanWithIllegalArgumentException() {
+        try {
+            XmlReplyBean result = new XmlReplyBean(-1, new ErrorBean(), new MessageBean());
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+        }
+    }
+
+    /**
+     * Test of toXml method, of class XmlReplyBean.
+     */
+    @Test
+    public void testToXml() {
+        XmlReplyBean instance = new XmlReplyBean();
+        instance.setMessage(new MessageBean("TEST"));
+        String result = instance.toXml();
+        assertTrue(result.contains("TEST"));
+    }
+
+    /**
+     * Test of setAcknowledgment method, of class XmlReplyBean.
+     */
+    @Test
+    public void testSetAcknowledgment() {
+        AckBean acknowledgment = new AckBean(1);
+        XmlReplyBean instance = new XmlReplyBean();
+        instance.setAcknowledgment(acknowledgment);
+        assertEquals("OK", instance.getAcknowledgment().getStatus());
+    }
+
+    /**
+     * Test of getAcknowledgment method, of class XmlReplyBean.
+     */
+    @Test
+    public void testGetAcknowledgment() {
+        AckBean acknowledgment = new AckBean(1);
+        XmlReplyBean instance = new XmlReplyBean();
+        instance.setAcknowledgment(acknowledgment);
+        AckBean result = instance.getAcknowledgment();
+        assertEquals("OK", result.getStatus());
+
+    }
+
+    /**
+     * Test of setContent method, of class XmlReplyBean.
+     */
+    @Test
+    public void testSetContent() {
+        XmlBean[] content = {null, null};
+        XmlReplyBean instance = new XmlReplyBean();
+        instance.setContent(content);
+        assertEquals(2, instance.getContent().length);
+    }
+
+    /**
+     * Test of getContent method, of class XmlReplyBean.
+     */
+    @Test
+    public void testGetContent() {
+        XmlReplyBean instance = new XmlReplyBean();
+        instance.setContent(new XmlBean[] {null, null});        
+        Object[] result = instance.getContent();
+        assertEquals(2, result.length);
+    }
+
+    /**
+     * Test of setMessage method, of class XmlReplyBean.
+     */
+    @Test
+    public void testSetMessage() {
+        MessageBean message = new MessageBean("TEST");
+        XmlReplyBean instance = new XmlReplyBean();
+        instance.setMessage(message);
+        assertEquals("TEST", instance.getMessage().getMessages().get(0));
+    }
+
+    /**
+     * Test of getMessage method, of class XmlReplyBean.
+     */
+    @Test
+    public void testGetMessage() {
+        MessageBean message = new MessageBean("TEST");
+        XmlReplyBean instance = new XmlReplyBean();
+        instance.setMessage(message);
+        assertEquals("TEST", instance.getMessage().getMessages().get(0));
+    }
+
+    /**
+     * Test of getErrorEnum method, of class XmlReplyBean.
+     */
+    @Test
+    public void testGetErrorEnum() {
+        XmlReplyBean instance = new XmlReplyBean();
+        instance.setErrorEnum(ErrorEnum.ERR_ATTRIBUTES_NOT_FOUND);
+        ErrorEnum expResult = ErrorEnum.ERR_ATTRIBUTES_NOT_FOUND;
+        ErrorEnum result = instance.getErrorEnum();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setErrorEnum method, of class XmlReplyBean.
+     */
+    @Test
+    public void testSetErrorEnum() {
+        XmlReplyBean instance = new XmlReplyBean();
+        instance.setErrorEnum(ErrorEnum.ERR_ATTRIBUTES_NOT_FOUND);
+        ErrorEnum expResult = ErrorEnum.ERR_ATTRIBUTES_NOT_FOUND;
+        ErrorEnum result = instance.getErrorEnum();
+        assertEquals(expResult, result);
+    }
+}
--- src/test/java/gov/usgs/gdp/interfaces/geotools/AnalyzeFileTest.java
+++ src/test/java/gov/usgs/gdp/interfaces/geotools/AnalyzeFileTest.java
@@ -19,229 +19,226 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-
 public class AnalyzeFileTest {
-	
-	private static final String testFile = "demo_HUCs";
-	
-	private static org.apache.log4j.Logger log = Logger.getLogger(AnalyzeFileTest.class);
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		log.debug("Started testing class");
-	} 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		log.debug("Ended testing class");
-	}
-	private String tempDir = "";
 
-	private String seperator = "";
-	
-	@Before
-	public void setUp() throws Exception {
-		this.tempDir = System.getProperty("java.io.tmpdir");
+    private static final String testFile = "demo_HUCs";
+    private static org.apache.log4j.Logger log = Logger.getLogger(AnalyzeFileTest.class);
 
-		if ( !(this.tempDir.endsWith("/") || this.tempDir.endsWith("\\")) )
-		   this.tempDir = this.tempDir + System.getProperty("file.separator");
-		
-		String systemTempDir = System.getProperty("java.io.tmpdir"); 
-		this.seperator =  java.io.File.separator;
-		String currentTime = Long.toString((new Date()).getTime());
-		this.tempDir = systemTempDir + this.seperator + currentTime;
-		(new File(this.tempDir)).mkdir();
-		
-		// Copy example files 
-		ClassLoader cl = Thread.currentThread().getContextClassLoader(); 
-		URL sampleFileLocation = cl.getResource("Sample_Files/");
-		if (sampleFileLocation != null) {
-			File sampleFiles = null;
-			try {
-				sampleFiles = new File(sampleFileLocation.toURI());
-			} catch (URISyntaxException e) {
-				assertTrue("Exception encountered: " + e.getMessage(), false);
-			}
-			FileHelper.copyFileToFile(sampleFiles, this.tempDir + this.seperator);
-		} else {
-			assertTrue("Sample files could not be loaded for test", false);
-		}
-	}
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        log.debug("Started testing class");
+    }
 
-	@After
-	public void tearDown() throws Exception {
-		FileUtils.deleteDirectory((new File(this.tempDir)));
-	}
-	
-	@Test
-	public void createClass() {
-		AnalyzeFile result = new AnalyzeFile();
-		assertNotNull(result);
-	}
-	
-	@Test 
-	public void testGetFileSummaryFromShapeFileSetBean() {
-		String shpFile = this.tempDir 
-		+ this.seperator 
-		+ "Sample_Files" 
-		+ this.seperator
-		+ "Shapefiles" 
-		+ this.seperator
-		+ testFile + ".shp";
-		
-		String dbfFile = this.tempDir 		
-		+ this.seperator 
-		+ "Sample_Files" 
-		+ this.seperator
-		+ "Shapefiles" 
-		+ this.seperator
-		+ testFile + ".dbf";
-		
-		List<String> result = null;
-		
-		ShapeFileSetBean testBean = new ShapeFileSetBean();
-		testBean.setDbfFile(new File(dbfFile));
-		testBean.setShapeFile(new File(shpFile));
-		
-		try {
-			result = AnalyzeFile.getFileSummary(testBean);
-			assertNotNull(result);
-			assertFalse(result.isEmpty());
-		} catch (IOException e1) {
-			fail(e1.getMessage());
-		}
-		
-		
-		testBean.setDbfFile(null);
-		testBean.setShapeFile(null);
-		try {
-			result = AnalyzeFile.getFileSummary(testBean);
-			assertNotNull(result);
-			assertTrue(result.isEmpty());
-		} catch (IOException e1) {
-			fail(e1.getMessage());
-		}
-		
-		
-		File zeroByteFile = null;
-		try {
-			zeroByteFile = File.createTempFile("temp", "tmp");
-		} catch (IOException e) {
-			fail(e.getMessage());
-		}
-		
-		testBean.setDbfFile(zeroByteFile);
-		testBean.setShapeFile(zeroByteFile);
-		try {
-			result = AnalyzeFile.getFileSummary(testBean);
-			assertNotNull(result);
-			assertTrue(result.isEmpty());
-		} catch (IOException e) {
-			fail(e.getMessage());
-		}
-		
-		
-		testBean.setDbfFile(new File("does/not/exist"));
-		testBean.setShapeFile(new File("does/not/exist"));
-		try {
-			result = AnalyzeFile.getFileSummary(testBean);
-			assertNotNull(result);
-			assertTrue(result.isEmpty());
-		} catch (IOException e) {
-			fail(e.getMessage());
-		}
-		
-	}
-	
-	@Test 
-	public void testGetFileSummary() {
-		String shpFile = this.tempDir 
-		+ this.seperator 
-		+ "Sample_Files" 
-		+ this.seperator
-		+ "Shapefiles" 
-		+ this.seperator
-		+ testFile + ".shp";
-		
-		String dbfFile = this.tempDir 		
-		+ this.seperator 
-		+ "Sample_Files" 
-		+ this.seperator
-		+ "Shapefiles" 
-		+ this.seperator
-		+ testFile + ".dbf";
-		
-		File DBFFILE = new File(dbfFile);
-		File SHPFILE = new File(shpFile);
-		
-		List<String> result;
-		try {
-			result = AnalyzeFile.getFileSummary(new File("does/not/exist"));
-			assertNotNull(result);
-			assertTrue(result.isEmpty());
-		} catch (IOException e1) {
-			fail(e1.getMessage());
-		}
-		
-		
-		try {
-			result = AnalyzeFile.getFileSummary(dbfFile);
-			assertNotNull(result);
-			assertFalse(result.isEmpty());
-		} catch (IOException e1) {
-			fail(e1.getMessage());
-		}
-		
-		
-		try {
-			result = AnalyzeFile.getFileSummary(shpFile);
-			assertNotNull(result);
-			assertFalse(result.isEmpty());
-		} catch (IOException e1) {
-			fail(e1.getMessage());
-		}
-		
-		
-		try {
-			result = AnalyzeFile.getFileSummary(DBFFILE);
-			assertNotNull(result);
-			assertFalse(result.isEmpty());
-		} catch (IOException e1) {
-			fail(e1.getMessage());
-		}
-		
-		
-		try {
-			result = AnalyzeFile.getFileSummary(SHPFILE);
-			assertNotNull(result);
-			assertFalse(result.isEmpty());
-		} catch (IOException e1) {
-			fail(e1.getMessage());
-		}
-		
-		
-		try {
-			File zeroByteFile = File.createTempFile("temp", "tmp");
-			result = AnalyzeFile.getFileSummary(zeroByteFile);
-			assertNotNull(result);
-			assertTrue(result.isEmpty());
-		} catch (IOException e) {
-			fail(e.getMessage());
-		}
-		
-		File directory = new File(this.tempDir 		
-				+ this.seperator 
-				+ "Sample_Files" 
-				+ this.seperator
-				+ "Shapefiles" 
-				+ this.seperator);
-		try {
-			result = AnalyzeFile.getFileSummary(directory);
-			assertNotNull(result);
-			assertTrue(result.isEmpty());
-		} catch (IOException e) {
-			fail(e.getMessage());
-		}
-		
-		
-		
-	}
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        log.debug("Ended testing class");
+    }
+    private String tempDir = "";
+    private String seperator = "";
+
+    @Before
+    public void setUp() throws Exception {
+        this.tempDir = System.getProperty("java.io.tmpdir");
+
+        if (!(this.tempDir.endsWith("/") || this.tempDir.endsWith("\\"))) {
+            this.tempDir = this.tempDir + System.getProperty("file.separator");
+        }
+
+        String systemTempDir = System.getProperty("java.io.tmpdir");
+        this.seperator = java.io.File.separator;
+        String currentTime = Long.toString((new Date()).getTime());
+        this.tempDir = systemTempDir + this.seperator + currentTime;
+        (new File(this.tempDir)).mkdir();
+
+        // Copy example files
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        URL sampleFileLocation = cl.getResource("Sample_Files/");
+        if (sampleFileLocation != null) {
+            File sampleFiles = null;
+            try {
+                sampleFiles = new File(sampleFileLocation.toURI());
+            } catch (URISyntaxException e) {
+                assertTrue("Exception encountered: " + e.getMessage(), false);
+            }
+            FileHelper.copyFileToFile(sampleFiles, this.tempDir + this.seperator);
+        } else {
+            assertTrue("Sample files could not be loaded for test", false);
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        FileUtils.deleteDirectory((new File(this.tempDir)));
+    }
+
+    @Test
+    public void createClass() {
+        AnalyzeFile result = new AnalyzeFile();
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testGetFileSummaryFromShapeFileSetBean() {
+        String shpFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".shp";
+
+        String dbfFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".dbf";
+
+        List<String> result = null;
+
+        ShapeFileSetBean testBean = new ShapeFileSetBean();
+        testBean.setDbfFile(new File(dbfFile));
+        testBean.setShapeFile(new File(shpFile));
+
+        try {
+            result = AnalyzeFile.getFileSummary(testBean);
+            assertNotNull(result);
+            assertFalse(result.isEmpty());
+        } catch (IOException e1) {
+            fail(e1.getMessage());
+        }
+
+
+        testBean.setDbfFile(null);
+        testBean.setShapeFile(null);
+        try {
+            result = AnalyzeFile.getFileSummary(testBean);
+            assertNotNull(result);
+            assertTrue(result.isEmpty());
+        } catch (IOException e1) {
+            fail(e1.getMessage());
+        }
+
+
+        File zeroByteFile = null;
+        try {
+            zeroByteFile = File.createTempFile("temp", "tmp");
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+
+        testBean.setDbfFile(zeroByteFile);
+        testBean.setShapeFile(zeroByteFile);
+        try {
+            result = AnalyzeFile.getFileSummary(testBean);
+            assertNotNull(result);
+            assertTrue(result.isEmpty());
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+
+
+        testBean.setDbfFile(new File("does/not/exist"));
+        testBean.setShapeFile(new File("does/not/exist"));
+        try {
+            result = AnalyzeFile.getFileSummary(testBean);
+            assertNotNull(result);
+            assertTrue(result.isEmpty());
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void testGetFileSummary() {
+        String shpFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".shp";
+
+        String dbfFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".dbf";
+
+        File DBFFILE = new File(dbfFile);
+        File SHPFILE = new File(shpFile);
+
+        List<String> result;
+        try {
+            result = AnalyzeFile.getFileSummary(new File("does/not/exist"));
+            assertNotNull(result);
+            assertTrue(result.isEmpty());
+        } catch (IOException e1) {
+            fail(e1.getMessage());
+        }
+
+
+        try {
+            result = AnalyzeFile.getFileSummary(dbfFile);
+            assertNotNull(result);
+            assertFalse(result.isEmpty());
+        } catch (IOException e1) {
+            fail(e1.getMessage());
+        }
+
+
+        try {
+            result = AnalyzeFile.getFileSummary(shpFile);
+            assertNotNull(result);
+            assertFalse(result.isEmpty());
+        } catch (IOException e1) {
+            fail(e1.getMessage());
+        }
+
+
+        try {
+            result = AnalyzeFile.getFileSummary(DBFFILE);
+            assertNotNull(result);
+            assertFalse(result.isEmpty());
+        } catch (IOException e1) {
+            fail(e1.getMessage());
+        }
+
+
+        try {
+            result = AnalyzeFile.getFileSummary(SHPFILE);
+            assertNotNull(result);
+            assertFalse(result.isEmpty());
+        } catch (IOException e1) {
+            fail(e1.getMessage());
+        }
+
+
+        try {
+            File zeroByteFile = File.createTempFile("temp", "tmp");
+            result = AnalyzeFile.getFileSummary(zeroByteFile);
+            assertNotNull(result);
+            assertTrue(result.isEmpty());
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+
+        File directory = new File(this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator);
+        try {
+            result = AnalyzeFile.getFileSummary(directory);
+            assertNotNull(result);
+            assertTrue(result.isEmpty());
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+    }
 }
