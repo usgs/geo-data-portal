e732772ebc215b65889d37d691a43e781c9a17bf
razoerb <razoerb@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2010-08-20 20:27:29 +0000

    Add to data-access
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@10966 622dc1fb-2224-0410-ab72-dc85a3975e1a

---
+++ gdp-data-access/src/main/java/gov/usgs/cida/gdp/dataaccess/bean/ServerBean.java
@@ -0,0 +1,106 @@
+package gov.usgs.cida.gdp.dataaccess.bean;
+
+import gov.usgs.cida.gdp.utilities.bean.XmlBean;
+import java.util.Date;
+
+import org.apache.log4j.Logger;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+@XStreamAlias("server")
+public class ServerBean implements XmlBean{
+
+	private static org.apache.log4j.Logger log = Logger.getLogger(ServerBean.class);
+	
+        private String name;
+	private String protocol;
+	private String hostname;
+	private int port;
+	private String uri;
+	private boolean active;
+	private Date lastCheck;
+        private String fullUrl;
+	
+	public static org.apache.log4j.Logger getLog() {
+		return log;
+	}
+	public static void setLog(org.apache.log4j.Logger log) {
+		ServerBean.log = log;
+	}
+	public String getHostname() {
+		return hostname;
+	}
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	public Date getLastCheck() {
+		return lastCheck;
+	}
+	public void setLastCheck(Date lastCheck) {
+		this.lastCheck = lastCheck;
+	}
+	@Override
+	public String toXml() {
+		XStream xstream = new XStream();
+		xstream.autodetectAnnotations(true);
+		StringBuffer sb = new StringBuffer();
+		String result = "";
+		sb.append(xstream.toXML(this));
+		result = sb.toString();
+		return result;
+	}
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+	public String getUri() {
+		return uri;
+	}
+	public void setProtocol(String protocol) {
+		this.protocol = protocol;
+	}
+	public String getProtocol() {
+		return protocol;
+	}
+
+    /**
+     * @return the fullUrl
+     */
+    public String getFullUrl() {
+        return fullUrl;
+    }
+
+    /**
+     * @param fullUrl the fullUrl to set
+     */
+    public void setFullUrl(String fullUrl) {
+        this.fullUrl = fullUrl;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+	
+}
---
+++ gdp-data-access/src/main/java/gov/usgs/cida/gdp/dataaccess/helper/THREDDSServerHelper.java
@@ -0,0 +1,278 @@
+package gov.usgs.cida.gdp.dataaccess.helper;
+
+import gov.usgs.cida.gdp.dataaccess.bean.DataTypeCollectionBean;
+import gov.usgs.cida.gdp.dataaccess.bean.ServerBean;
+import gov.usgs.cida.gdp.dataaccess.bean.THREDDSInfoBean;
+import gov.usgs.cida.gdp.utilities.bean.TimeBean;
+import gov.usgs.cida.gdp.utilities.bean.XmlBean;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.URI;
+import java.net.UnknownHostException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Formatter;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TimerTask;
+import java.util.TreeMap;
+import javax.servlet.ServletConfig;
+import org.apache.log4j.Logger;
+
+import thredds.catalog.*;
+import ucar.nc2.VariableSimpleIF;
+import ucar.nc2.ft.FeatureDataset;
+import ucar.nc2.ft.FeatureDatasetFactoryManager;
+
+public class THREDDSServerHelper {
+
+    static org.apache.log4j.Logger log = Logger.getLogger(THREDDSServerHelper.class);
+
+    /**
+     * Tests whether or not a THREDDS server is reachable
+     *
+     * @param host
+     * @param port
+     * @param timeout - milliseconds
+     * @return
+     * @throws IOException
+     */
+    public static boolean isServerReachable(final String host, final int port, final int timeout) throws IOException {
+        boolean result = false;
+
+        Socket testSocket = new Socket();
+        InetSocketAddress address = new InetSocketAddress(host, port);
+        testSocket.connect(address, timeout);
+        result = testSocket.isConnected();
+        if (result) {
+            testSocket.close();
+        }
+
+        return result;
+    }
+
+    public static TimeBean getTimeBean(String datasetUrl, String gridSelection) throws IOException, ParseException {
+        Formatter errorLog = new Formatter();
+        FeatureDataset featureDataset = null;
+        featureDataset =
+                FeatureDatasetFactoryManager.open(null, datasetUrl, null, errorLog);
+
+        if (featureDataset != null) {
+            try {
+
+                TimeBean timeBean = getTimeBean(datasetUrl, gridSelection);
+                if (timeBean != null /*&& !timeBean.getTime().isEmpty()*/) {
+                    return timeBean;
+                }
+                return null;
+            } finally {
+                featureDataset.close();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Returns a list of dataset handles from the specified server.
+     * 
+     * @param hostname
+     * @param port
+     * @param uri
+     * @param serviceType
+     * @return
+     */
+    public static List<InvAccess> getDatasetHandlesFromServer(
+            String hostname, int port, String uri, ServiceType serviceType) {
+        try {
+            InvCatalog catalog = getCatalogFromServer(hostname, port, uri);
+            return NetCDFUtility.getDatasetHandles(catalog, serviceType);
+        } catch (IOException e) {
+            return Collections.emptyList();
+        }
+    }
+
+    public static InvCatalog getCatalogFromServer(String hostname, int port, String uri) throws IOException {
+        String ThreddsURL = "http://" + hostname + ":" + port + uri;
+        URI catalogURI = URI.create(ThreddsURL);
+        InvCatalogFactory factory = new InvCatalogFactory("default", true);
+        InvCatalog catalog = factory.readXML(catalogURI);
+
+        StringBuilder buff = new StringBuilder();
+        if (!catalog.check(buff)) {
+            throw new IOException(buff.toString());
+        }
+
+        return catalog;
+    }
+
+    /**
+     * Returns a List<XmlBean> which is actually a List<DataSetBean>
+     * 
+     * @param hostname
+     * @param port
+     * @param uri
+     * @return
+     * @throws IllegalArgumentException
+     */
+    /*public static List<XmlBean> getDatasetListFromServer(String hostname,
+    int port, String uri) throws IllegalArgumentException {
+
+    if (hostname == null || "".equals(hostname)) throw new IllegalArgumentException("Hostname invalid or null");
+    if (uri == null || "".equals(uri)) throw new IllegalArgumentException("URI invalid or null");
+    if (port == 0) port = 80;
+
+    List<XmlBean> result = new ArrayList<XmlBean>();
+    List<InvAccess> invAccessList = THREDDSServerHelper.getDatasetHandlesFromServer(hostname, port, uri);
+
+    if (invAccessList == null) return result;
+    for (InvAccess invAccess : invAccessList) {
+
+    InvDataset ds = invAccess.getDataset();
+    List<InvPropertyBean> = ds.getP
+    DataSetBean dsb = new DataSetBean(ds);
+    result.add(dsb);
+    }
+
+
+    return result;
+    }*/
+    public static List<XmlBean> getGridBeanListFromServer(String datasetUrl) throws IllegalArgumentException, IOException {
+
+        if (datasetUrl == null || "".equals(datasetUrl)) {
+            throw new IllegalArgumentException("DataSet URL invalid or null");
+        }
+
+        List<XmlBean> result = new ArrayList<XmlBean>();
+        List<VariableSimpleIF> variables = NetCDFUtility.getDataVariableNames(datasetUrl);
+        String type = NetCDFUtility.getDatasetType(datasetUrl);
+        DataTypeCollectionBean dtcb = new DataTypeCollectionBean(type, variables.toArray(new VariableSimpleIF[0]));
+        result.add(dtcb);
+        return result;
+
+    }
+
+     public final class testTHREDDSServers extends TimerTask {
+
+        private ServletConfig paramConfig;
+
+        public testTHREDDSServers(@SuppressWarnings("hiding") ServletConfig paramConfig) {
+            super();
+            setParamConfig(paramConfig);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public void run() {
+            Map<String, ServerBean> threddsServerBeanMap = (Map<String, ServerBean>) this.paramConfig.getServletContext().getAttribute("threddsServerBeanMap");
+            if (threddsServerBeanMap == null) {
+                threddsServerBeanMap = getTHREDDSServerBeanMap();
+            }
+
+            threddsServerBeanMap = checkServers(threddsServerBeanMap);
+            this.paramConfig.getServletContext().setAttribute("threddsServerBeanMap", threddsServerBeanMap);
+        }
+
+        /**
+         * Check the Map of servers to see if they're up or down
+         *
+         * @param threddsServerBeanMap
+         * @return
+         */
+        private Map<String, ServerBean> checkServers(Map<String, ServerBean> threddsServerBeanMap) {
+            Map<String, ServerBean> result = new TreeMap<String, ServerBean>();
+
+            Set<String> threddsServerBeanMapKeySet = threddsServerBeanMap.keySet();
+            Iterator<String> threddsServerBeanMapKeySetIterator = threddsServerBeanMapKeySet.iterator();
+
+            while (threddsServerBeanMapKeySetIterator.hasNext()) {
+                String key = threddsServerBeanMapKeySetIterator.next();
+                ServerBean threddsServerBean = threddsServerBeanMap.get(key);
+                threddsServerBean.setLastCheck(new Date());
+                String host = threddsServerBean.getHostname();
+                int port = threddsServerBean.getPort();
+                int timeout = 5000;
+
+                boolean serverIsUp = false;
+                try {
+                    serverIsUp = THREDDSServerHelper.isServerReachable(host, port, timeout);
+                } catch (UnknownHostException e) {
+                    log.debug("Host " + host + ":" + port + " could not be reached. Reason: " + e.getMessage() + "\n\tBeing labeled as down. Will re-check in 5 minutes.");
+                } catch (IOException e) {
+                    log.debug("Host " + host + ":" + port + " could not be reached. Reason: " + e.getMessage() + "\n\tBeing labeled as down. Will re-check in 5 minutes.");
+                }
+                threddsServerBean.setActive(serverIsUp);
+                result.put(key, threddsServerBean);
+            }
+
+            return result;
+        }
+
+        private Map<String, ServerBean> getTHREDDSServerBeanMap() {
+            Map<String, ServerBean> result = new TreeMap<String, ServerBean>();
+
+            Map<String, String> threddsUrlMap = THREDDSInfoBean.getTHREDDSUrlMap();
+            Set<String> threddsUrlMapKeySet = threddsUrlMap.keySet();
+            Iterator<String> threddsUrlMapKeySetIterator = threddsUrlMapKeySet.iterator();
+            while (threddsUrlMapKeySetIterator.hasNext()) {
+                String key = threddsUrlMapKeySetIterator.next();
+                String name = key;
+                String serverUrl = threddsUrlMap.get(key);
+                String protocol;
+                ServerBean threddsServerBean = new ServerBean();
+
+                int startAt = 0;
+                if (serverUrl.contains("http:")) {
+                    startAt = 7;
+                    protocol = "http://";
+                } else {
+                    startAt = 8;
+                    protocol = "https://";
+                }
+
+                String hostname = "";
+                boolean hasPort = true;
+                try {
+                    hostname = serverUrl.substring(startAt, serverUrl.indexOf(':', startAt));
+                } catch (StringIndexOutOfBoundsException e) {
+                    // Has no port
+                    hostname = serverUrl.substring(startAt, serverUrl.indexOf('/', startAt));
+                    hasPort = false;
+                }
+
+                String port = "80";
+                startAt = hostname.length() + startAt;
+                if (hasPort) {
+                    port = serverUrl.substring(startAt + 1, serverUrl.indexOf("/", startAt));
+                    startAt = startAt + port.length() + 1;
+                }
+
+                String uri = "";
+
+                uri = serverUrl.substring(startAt);
+                threddsServerBean.setName(name);
+                threddsServerBean.setUri(uri);
+                threddsServerBean.setProtocol(protocol);
+                threddsServerBean.setHostname(hostname);
+                threddsServerBean.setPort(Integer.parseInt(port));
+                threddsServerBean.setFullUrl(serverUrl);
+                result.put(key, threddsServerBean);
+            }
+
+            return result;
+        }
+
+        public ServletConfig getParamConfig() {
+            return this.paramConfig;
+        }
+
+        public void setParamConfig(@SuppressWarnings("hiding") ServletConfig paramConfig) {
+            this.paramConfig = paramConfig;
+        }
+    }
+}
