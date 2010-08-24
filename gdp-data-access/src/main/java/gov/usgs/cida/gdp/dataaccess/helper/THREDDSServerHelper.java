38bc2322f94a1f345c1b2b24a3a5de0018bdd59b
razoerb <razoerb@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2010-08-24 22:14:40 +0000

    Refactored GDP now mostly working.
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@11000 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- gdp-data-access/src/main/java/gov/usgs/cida/gdp/dataaccess/helper/THREDDSServerHelper.java
+++ gdp-data-access/src/main/java/gov/usgs/cida/gdp/dataaccess/helper/THREDDSServerHelper.java
@@ -65,19 +65,29 @@ public class THREDDSServerHelper {
 
         if (featureDataset != null) {
             try {
-
-                TimeBean timeBean = getTimeBean(datasetUrl, gridSelection);
+                List<String> dateRange = NetCDFUtility.getDateRange(datasetUrl, gridSelection);
+        		if (dateRange.isEmpty()) {
+        			boolean hasTimeCoord = NetCDFUtility.hasTimeCoordinate(datasetUrl);
+        			if (hasTimeCoord) { // This occurs when there is no date range in the file but has time coords
+        				// We want the user to pick dates but don't have a range to give 
+        				dateRange.add("1800-01-01 00:00:00Z");
+        				dateRange.add("2100-12-31 00:00:00Z");
+        			}
+        		}
+        		
+        		TimeBean timeBean = new TimeBean(dateRange);
+        		
                 if (timeBean != null /*&& !timeBean.getTime().isEmpty()*/) {
                     return timeBean;
                 }
-                return null;
+                
             } finally {
                 featureDataset.close();
             }
         }
         return null;
     }
-
+    
     /**
      * Returns a list of dataset handles from the specified server.
      * 
