db7db9abbcc111bae2ae7c35008b669b571d14ab
jiwalker <jiwalker@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2010-08-25 22:02:09 +0000

    Move epa nhd query from core-processing:ProcessServlet to mapping:NHDService.  Move sendPacket and other http utils into common-utilities:HTTPUtils
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@11043 622dc1fb-2224-0410-ab72-dc85a3975e1a

---
+++ gdp-common-utilities/src/main/java/gov/usgs/cida/gdp/utilities/HTTPUtils.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.usgs.cida.gdp.utilities;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author admin
+ */
+public class HTTPUtils {
+
+	public static InputStream sendPacket(URL url, String requestMethod)
+			throws IOException {
+
+		HttpURLConnection httpConnection = openHttpConnection(url,
+				requestMethod);
+
+		return getHttpConnectionInputStream(httpConnection);
+	}
+
+	public static HttpURLConnection openHttpConnection(URL url,
+			String requestMethod) throws IOException, ProtocolException {
+
+		HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
+		httpConnection.setRequestMethod(requestMethod);
+
+		return httpConnection;
+	}
+
+	public static InputStream getHttpConnectionInputStream(HttpURLConnection httpConnection)
+			throws IOException {
+
+		return httpConnection.getInputStream();
+	}
+
+	public static Map<String, List<String>> getHttpConnectionHeaderFields(HttpURLConnection httpConnection)
+			throws IOException {
+
+		return httpConnection.getHeaderFields();
+	}
+
+}
--- gdp-core-processing/src/main/java/gov/usgs/cida/gdp/coreprocessing/servlet/ProcessServlet.java
+++ gdp-core-processing/src/main/java/gov/usgs/cida/gdp/coreprocessing/servlet/ProcessServlet.java
@@ -16,10 +16,7 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringReader;
 import java.net.HttpURLConnection;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -38,33 +35,21 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.slf4j.LoggerFactory;
-import org.codehaus.jackson.JsonFactory;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.JsonParser;
 import org.geotools.data.DefaultQuery;
 import org.geotools.data.FeatureSource;
 import org.geotools.data.FileDataStore;
 import org.geotools.data.FileDataStoreFinder;
-import org.geotools.data.shapefile.shp.ShapeType;
-import org.geotools.data.shapefile.shp.ShapefileWriter;
 import org.geotools.feature.FeatureCollection;
-import org.geotools.feature.FeatureCollections;
 import org.geotools.feature.SchemaException;
-import org.geotools.feature.simple.SimpleFeatureBuilder;
-import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
 import org.geotools.filter.text.cql2.CQL;
 import org.geotools.filter.text.cql2.CQLException;
-import org.geotools.referencing.CRS;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.filter.Filter;
 import org.opengis.referencing.FactoryException;
-import org.opengis.referencing.NoSuchAuthorityCodeException;
 import org.opengis.referencing.operation.TransformException;
-import org.xml.sax.SAXException;
 
 import ucar.ma2.InvalidRangeException;
 import ucar.ma2.Range;
@@ -79,14 +64,14 @@ import ucar.nc2.ft.FeatureDatasetPoint;
 import ucar.nc2.ft.StationTimeSeriesFeatureCollection;
 import ucar.nc2.units.DateRange;
 
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.GeometryCollection;
 import gov.usgs.cida.gdp.communication.EmailHandler;
 import gov.usgs.cida.gdp.communication.bean.EmailMessageBean;
 import gov.usgs.cida.gdp.coreprocessing.analysis.StationDataCSVWriter;
 import gov.usgs.cida.gdp.coreprocessing.analysis.grid.FeatureCoverageWeightedGridStatistics;
 import gov.usgs.cida.gdp.coreprocessing.analysis.grid.FeatureCoverageWeightedGridStatisticsWriter.Statistic;
 import gov.usgs.cida.gdp.coreprocessing.servlet.ProcessServlet.GroupBy.StationOption;
+import gov.usgs.cida.gdp.mapping.NHDService;
+import gov.usgs.cida.gdp.utilities.HTTPUtils;
 import ucar.nc2.NetcdfFile;
 
 
@@ -277,59 +262,13 @@ public class ProcessServlet extends HttpServlet {
     	String shapefilePath = null;
 
 	    if (lat != null && lon != null) {
+
+            // When getting the geometry from the epa nhd service, there won't be
+            // any attributes.  But the attribute cannot be null, so set it to a
+            // placeholder, "blah"
+            attributeName = "blah";
 	    	
-	    	// Get reachcode containing lat/lon point from the EPA WATERS web service
-	    	InputStream reachJson =
-	    		sendPacket(new URL("http://iaspub.epa.gov/waters10/waters_services.PointIndexingService?" +
-	    			"pGeometry=POINT(" + lon + "%20" + lat + ")" + "&pGeometryMod=WKT,SRID=8307" +
-	                "&pPointIndexingMethod=RAINDROP" + "&pPointIndexingRaindropDist=25"), "GET");
-
-	    	String reachCode = parseJSON(reachJson, "reachcode");
-
-	    	// Get geometry of reachcode
-    		InputStream json = 
-    			sendPacket(new URL("http://iaspub.epa.gov/waters10/waters_services.navigationDelineationService?" +
-    				"pNavigationType=UT&pStartReachCode=" + reachCode + "&optOutGeomFormat=GEOGML&pFeatureType=CATCHMENT_TOPO&pMaxDistance=999999999"),
-    				"GET");
-
-    		String gml = parseJSON(json, "shape");
-
-    		attributeName = "blah";
-    		
-    		String fullUserDir = System.getProperty("applicationUserSpaceDir") +
-    				userDirectory + FileHelper.getSeparator();
-    		
-    		
-    		try {
-    			GeometryCollection g = parseGML(gml);
-    			
-    			// Write to a shapefile so GeoServer can load the geometry
-    			shapefilePath = fullUserDir + "latlon.shp";
-    			
-    			File shpFile = new File(shapefilePath);
-    			File shxFile = new File(fullUserDir + "latlon.shx");
-    			
-    			if (shpFile.exists()) shpFile.delete();
-    			if (shxFile.exists()) shxFile.delete();
-    			
-    			shpFile.createNewFile();
-    			shxFile.createNewFile();
-    			
-    			FileOutputStream shpFileInputStream = new FileOutputStream(shpFile);
-    			FileOutputStream shxFileInputStream = new FileOutputStream(shxFile);
-    			ShapefileWriter sw = new ShapefileWriter(shpFileInputStream.getChannel(), 
-    					shxFileInputStream.getChannel());
-    			sw.write(g, ShapeType.POLYGON);
-    			
-    			featureCollection = createFeatureCollection(g);
-    			
-			} catch (SAXException e) {
-				e.printStackTrace();
-				return null;
-			} catch (ParserConfigurationException e) {
-				e.printStackTrace();
-				return null;
-			}
+	    	featureCollection = NHDService.getGeometry(lon, lat, userDirectory, shapefilePath);
 
     	} else {
 
@@ -433,13 +372,13 @@ public class ProcessServlet extends HttpServlet {
 
                 // Call getCoverage
                 HttpURLConnection httpConnection =
-                    openHttpConnection(new URL(getCoverageRequest), "GET");
+                    HTTPUtils.openHttpConnection(new URL(getCoverageRequest), "GET");
 
                 coverageIStream =
-                    getHttpConnectionInputStream(httpConnection);
+                    HTTPUtils.getHttpConnectionInputStream(httpConnection);
 
                 Map<String, List<String>> headerFields =
-                    getHttpConnectionHeaderFields(httpConnection);
+                    HTTPUtils.getHttpConnectionHeaderFields(httpConnection);
 
                 // TODO: check for error response from server
 
@@ -735,117 +674,6 @@ public class ProcessServlet extends HttpServlet {
 		}
 		
 		return null;  // new line not found
-	}
-
-	private static InputStream sendPacket(URL url, String requestMethod)
-			throws IOException {
-
-		HttpURLConnection httpConnection = openHttpConnection(url,
-				requestMethod);
-
-		return getHttpConnectionInputStream(httpConnection);
-	}
-	
-	private static HttpURLConnection openHttpConnection(URL url,
-			String requestMethod) throws IOException, ProtocolException {
-		
-		HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
-		httpConnection.setRequestMethod(requestMethod);
-
-		return httpConnection;
-	}
-	
-	private static InputStream getHttpConnectionInputStream(HttpURLConnection httpConnection)
-			throws IOException {
-		
-		return httpConnection.getInputStream();
-	}
-	
-	private static Map<String, List<String>> getHttpConnectionHeaderFields(HttpURLConnection httpConnection)
-			throws IOException {
-	
-		return httpConnection.getHeaderFields();
-	}
-
-	private static String parseJSON(InputStream json, String element)
-	throws JsonParseException, IOException {
-
-		JsonFactory f = new JsonFactory();
-		JsonParser jp = f.createJsonParser(new InputStreamReader(json));
-
-		while (true) {
-			jp.nextToken();
-
-			if (!jp.hasCurrentToken())
-				break;
-
-			if (element.equals(jp.getCurrentName())) {
-				jp.nextToken();
-				return jp.getText();
-			}
-		}
-
-		System.out.println("\"" + element + "\" not found.");
-		return null;
-	}
-
-	private static GeometryCollection parseGML(String gml)
-	throws SchemaException, IOException, SAXException, ParserConfigurationException {
-
-		if (gml == null)
-			throw new IOException();
-
-		//create the parser with the gml 2.0 configuration
-		org.geotools.xml.Configuration configuration = new org.geotools.gml2.GMLConfiguration();
-		org.geotools.xml.Parser parser = new org.geotools.xml.Parser( configuration );
-
-
-		// TODO: parse fails with when gml is only a single polygon with "Authority "SDO" is unknown".
-		//parse
-		GeometryCollection geom = (GeometryCollection) parser.parse( new StringReader(gml) );
-
-		return geom;
-	}
-	
-	private static FeatureCollection<SimpleFeatureType, SimpleFeature> 
-	createFeatureCollection(GeometryCollection geom)
-	throws NoSuchAuthorityCodeException, FactoryException {
-		
-		FeatureCollection<SimpleFeatureType, SimpleFeature> fc = FeatureCollections.newCollection();
-
-		for (int i = 0; i < geom.getNumGeometries(); i++) {
-			Geometry g = geom.getGeometryN(i);
-
-			SimpleFeatureTypeBuilder typeBuilder = new SimpleFeatureTypeBuilder();
-			typeBuilder.setName("testType");
-			typeBuilder.setCRS(CRS.decode("EPSG:4326"));
-			typeBuilder.add("blah", Integer.class);
-			typeBuilder.add("geom", Geometry.class);
-			typeBuilder.setDefaultGeometry("geom");
-
-			SimpleFeatureType type = typeBuilder.buildFeatureType();
-
-
-//			GeometryFactory geomFactory = new GeometryFactory();
-			SimpleFeatureBuilder build = new SimpleFeatureBuilder( type );
-
-//			for (Coordinate c : g.getCoordinates()) {
-//				System.out.println(c.x + ", " + c.y);
-//				build.add( geomFactory.createPoint( c ));
-//			}
-
-			build.set("geom", g);
-			build.set("blah", i);
-
-			SimpleFeature sf = build.buildFeature(null);
-			sf.getBounds();
-
-//			SimpleFeature sf = SimpleFeatureBuilder.build(type, g.getCoordinates(), null);
-
-			fc.add(sf);
-		}
-
-		return fc;
 	}
 
 	private static final long serialVersionUID = 1L;
