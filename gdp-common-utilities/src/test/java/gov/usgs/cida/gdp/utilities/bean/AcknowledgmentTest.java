cd762cc9d2985526b61b9aa9be1c403d6ff4c0e7
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2010-09-02 14:19:01 +0000

    Acknowledgement is now 100% covered
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@11158 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- gdp-common-utilities/src/test/java/gov/usgs/cida/gdp/utilities/bean/AcknowledgmentTest.java
+++ gdp-common-utilities/src/test/java/gov/usgs/cida/gdp/utilities/bean/AcknowledgmentTest.java
@@ -5,18 +5,26 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import com.vividsolutions.jts.util.Assert;
-import gov.usgs.cida.gdp.utilities.bean.Acknowledgement;
 
 public class AcknowledgmentTest {
 
         @Test
-        public void testSetStatus() {
+        public void testSetStatusToOne() {
             Acknowledgement ackBean = new Acknowledgement(1);
             assertEquals("OK", ackBean.getStatus());
             ackBean.setStatus("FAIL");
             assertEquals("FAIL", ackBean.getStatus());
 
         }
+
+
+        @Test
+        public void testSetStatusToTwo() {
+            Acknowledgement ackBean = new Acknowledgement(2);
+            assertEquals("FAIL", ackBean.getStatus());
+
+        }
+
 
 	@Test
 	public void testInvalidStatus() {
