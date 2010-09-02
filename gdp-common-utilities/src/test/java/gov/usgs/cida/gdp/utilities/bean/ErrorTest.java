d2a3448ab2cb174dfa2bdc9a795507ab287cb0fd
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2010-09-02 15:40:17 +0000

    Error class is now 100% covered
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@11160 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- gdp-common-utilities/src/test/java/gov/usgs/cida/gdp/utilities/bean/ErrorTest.java
+++ gdp-common-utilities/src/test/java/gov/usgs/cida/gdp/utilities/bean/ErrorTest.java
@@ -1,58 +1,85 @@
 package gov.usgs.cida.gdp.utilities.bean;
 
-import gov.usgs.cida.gdp.utilities.bean.Error;
+import java.util.Date;
 import static org.junit.Assert.*;
 
-import java.util.Date;
 
 import org.junit.Test;
 
 public class ErrorTest {
+    @Test
+    public void testInitializeWithInteger() {
+        Error errBean = new Error(Integer.valueOf(0));
+        assertNotNull(errBean.getErrorMessage());
+        assertEquals(errBean.getErrorMessage(), "No Command Has Been Provided - To list all available commands, use command=commandlist");
+    }
 
+    @Test
+    public void testInitializeWithIntegerAndStacktrace() {
+        RuntimeException ex = new ArrayStoreException();
 
-//	@Test
-//	public void testToXmlWithBeanFilled() {
-//		Error errBean = new Error();
-//		errBean.setErrorClass("FakeName");
-//		errBean.setErrorCreated(new Date());
-//		errBean.setErrorMessage("Fake Message");
-//		errBean.setException(new Exception("Fake Exception"));
-//		
-//		String result = errBean.toXml();
-//		assertNotNull(result);
-//	}
-	
-//	@Test
-//	public void testToXmlWithSparselyFilledBean() {
-//		Error errBean = new Error();
-//		errBean.setErrorClass("FakeName");
-//		errBean.setErrorCreated(new Date());
-//		
-//		String result = errBean.toXml();
-//		assertNotNull(result);
-//	}
-	
-//	@Test
-//	public void testToXmlWithEmptyBean() {
-//		Error errBean = new Error();
-//		
-//		String result = errBean.toXml();
-//		assertNotNull(result);
-//	}
+        Error errBean = new Error(Integer.valueOf(0), ex);
+        assertNotNull(errBean.getErrorMessage());
+        assertEquals(errBean.getErrorMessage(), "No Command Has Been Provided - To list all available commands, use command=commandlist");
+        assertNotNull(errBean.getException());
+        assertEquals(errBean.getException().getClass(), ArrayStoreException.class);
+    }
 
-	@Test
-	public void testInitializeWithInteger() {
-		Error errBean = new Error(Integer.valueOf(0));
-		assertNotNull(errBean.getErrorMessage());
-		assertEquals(errBean.getErrorMessage(), "No Command Has Been Provided - To list all available commands, use command=commandlist");
-	}
-	
-//	@Test
-//	public void testInitializeWithIntegerXmlOutput() {
-//		Error errBean = new Error(Integer.valueOf(0));
-//		assertNotNull(errBean.getErrorMessage());
-//		assertEquals(errBean.getErrorMessage(), "No Command Has Been Provided - To list all available commands, use command=commandlist");
-//		String result = errBean.toXml();
-//		assertNotNull(result);
-//	}
+    @Test
+    public void testInitializeWithString() {
+        Error errBean = new Error("test");
+        assertNotNull(errBean.getErrorMessage());
+        assertEquals(errBean.getErrorMessage(), "test");
+    }
+
+    @Test
+    public void testSetGetMessage() {
+        Error errBean = new Error();
+        errBean.setErrorMessage("test");
+        assertNotNull(errBean.getErrorMessage());
+        assertEquals(errBean.getErrorMessage(), "test");
+    }
+
+    @Test
+    public void testSetGetException() {
+        RuntimeException ex = new ArrayStoreException();
+        Error errBean = new Error();
+        errBean.setException(ex);
+        assertNotNull(errBean.getException());
+        assertEquals(errBean.getException().getClass(), ArrayStoreException.class);
+    }
+
+    @Test
+    public void testSetGetErrorCreated() {
+        Error errBean = new Error();
+        Date date = new Date();
+        long longDate = date.getTime();
+        errBean.setErrorCreated(date);
+        assertNotNull(errBean.getErrorCreated());
+        assertEquals(errBean.getErrorCreated().getTime(), longDate);
+    }
+
+        @Test
+    public void testSetGetErrorClassParam() {
+        Error errBean = new Error();
+        errBean.setErrorClass("test");
+        assertNotNull(errBean.getErrorClass());
+        assertEquals(errBean.getErrorClass(), "test");
+    }
+
+        @Test
+        public void testToString() {
+            Error errBean = new Error();
+        errBean.setErrorClass("test");
+        assertNotNull(errBean.toString());
+        assertNotSame("", errBean.toString());
+        }
+
+                @Test
+        public void testSetGetErrorNumber() {
+            Error errBean = new Error();
+        errBean.setErrorNumber(Integer.MIN_VALUE);
+        assertNotNull(errBean.getErrorNumber());
+        assertTrue(errBean.getErrorNumber() == Integer.MIN_VALUE);
+        }
 }
