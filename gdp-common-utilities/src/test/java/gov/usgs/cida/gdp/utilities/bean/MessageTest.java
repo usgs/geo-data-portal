0736e868f9a17aed05a50d71d6dc2fc6261234e7
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2010-09-02 17:01:15 +0000

    Message is now 100% test covered
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@11164 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- gdp-common-utilities/src/test/java/gov/usgs/cida/gdp/utilities/bean/MessageTest.java
+++ gdp-common-utilities/src/test/java/gov/usgs/cida/gdp/utilities/bean/MessageTest.java
@@ -17,12 +17,31 @@ public class MessageTest {
 	}
 	
 	@Test
-	public void testGetMessages() {
+	public void testGetMessagesWithNullMessageParam() {
 		Message messageBean = new Message();
+                messageBean.setMessages(null);
 		List<String> result = messageBean.getMessages();
 		assertNotNull(result);
 		assertTrue(result.isEmpty());
-		
+	}
+        @Test
+	public void testAddMessageWithNullMessageParam() {
+		Message messageBean = new Message();
+                messageBean.setMessages(null);
+                messageBean.addMessage("test");
+		List<String> result = messageBean.getMessages();
+		assertNotNull(result);
+		assertFalse(result.isEmpty());
+	}
+
+
+        @Test
+	public void testGetMessagesWithMessagesInConstructor() {
+		Message messageBean = new Message("test1", "test2");
+		List<String> result = messageBean.getMessages();
+		assertNotNull(result);
+		assertFalse(result.isEmpty());
+                assertTrue(result.size() == 2);
 	}
 
 	@Test
