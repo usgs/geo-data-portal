22472b0fd25392355e0049facdc1c6a0fd58274f
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2010-09-02 15:17:11 +0000

    AvailableFiles is now 100% covered
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@11159 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- gdp-common-utilities/src/test/java/gov/usgs/cida/gdp/utilities/bean/AvailableFilesTest.java
+++ gdp-common-utilities/src/test/java/gov/usgs/cida/gdp/utilities/bean/AvailableFilesTest.java
@@ -1,14 +1,13 @@
 package gov.usgs.cida.gdp.utilities.bean;
 
-
-import gov.usgs.cida.gdp.utilities.bean.AvailableFiles;
 import static org.junit.Assert.*;
-import gov.usgs.cida.gdp.utilities.FileHelper;
 import gov.usgs.cida.gdp.utilities.FileHelper;
 
 import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
@@ -17,112 +16,166 @@ import org.junit.Test;
 
 public class AvailableFilesTest {
 
-	private String tempDir = "";
+    private String tempDir = "";
+    private String seperator = "";
 
-	private String seperator = "";
-	
-	@Before
-	public void setUp() throws Exception {
-		this.seperator = FileHelper.getSeparator();
-		this.tempDir = FileHelper.getSystemTemp() 
-			+ this.seperator 
-			+ "GDP-APP-TEMP" 
-			+ this.seperator
-			+ "testing-feel-free-to-delete"
-			+ this.seperator;
-		(new File(this.tempDir)).mkdir();
-		(new File(this.tempDir + "user_dir" + this.seperator)).mkdir();
-		
-		// Copy example files 
-		ClassLoader cl = Thread.currentThread().getContextClassLoader(); 
-		URL sampleFileLocation = cl.getResource("Sample_Files" + FileHelper.getSeparator());
-		if (sampleFileLocation != null) {
-			File sampleFiles = null;
-			try {
-				sampleFiles = new File(sampleFileLocation.toURI());
-			} catch (URISyntaxException e) {
-				assertTrue("Exception encountered: " + e.getMessage(), false);
-			}
-			FileHelper.copyFileToFile(sampleFiles, this.tempDir + this.seperator);
-			FileHelper.copyFileToFile(sampleFiles, this.tempDir + "user_dir" + this.seperator);
-		} else {
-			assertTrue("Sample files could not be loaded for test", false);
-		}
-	}
+    @Before
+    public void setUp() throws Exception {
+        this.seperator = FileHelper.getSeparator();
+        this.tempDir = FileHelper.getSystemTemp()
+                + this.seperator
+                + "GDP-APP-TEMP"
+                + this.seperator
+                + "testing-feel-free-to-delete"
+                + this.seperator;
+        (new File(this.tempDir)).mkdir();
+        (new File(this.tempDir + "user_dir" + this.seperator)).mkdir();
 
-
-	@After
-	public void tearDown() throws Exception {
-		FileUtils.deleteDirectory((new File(this.tempDir)));
-	}
-	
-	@Test
-	public void testGetAvailableFilesBeanWithEmptyStringDirectory() {
-		AvailableFiles result =
-			AvailableFiles.getAvailableFilesBean("", "");
-		
-		assertNull(result);
-	}
-
-	@Test
-	public void testGetAvailableFilesBeanWithNullDirectory() {
-		AvailableFiles result =
-			AvailableFiles.getAvailableFilesBean(null, null);
-		
-		assertNull(result);
-	}
-
-        @Test
-	public void testGetAvailableFilesBeanWithPopulatedExamplesDirectory() {
-		AvailableFiles result =
-			AvailableFiles.getAvailableFilesBean(this.tempDir, null);
-		
-		assertNotNull(result);
-		assertNotNull(result.getExampleFileList());
-		assertFalse(result.getExampleFileList().isEmpty());
-		assertNotNull(result.getExampleFileList());
-		assertFalse(result.getShapeSetList().isEmpty());
-		assertTrue(result.getUserFileList().isEmpty());
-	}
-	
-	@Test
-	public void testGetAvailableFilesBeanWithPopulatedUserDirectory() {
-		AvailableFiles result =
-			AvailableFiles.getAvailableFilesBean(this.tempDir,
-						this.tempDir + "user_dir"
-						+ this.seperator
-						+ "Sample_Files"
-						+ this.seperator
-						+ "Shapefiles"
-						+ this.seperator);
-		assertNotNull(result);
-		assertNotNull(result.getExampleFileList());
-		assertFalse(result.getExampleFileList().isEmpty());
-		assertNotNull(result.getExampleFileList());
-		assertFalse(result.getShapeSetList().isEmpty());
-		assertFalse(result.getUserFileList().isEmpty());
-		
-		
-	}
-
-        @Test
-	public void testGetAvailableFilesBeanWithBogusUserDirectory() {
+        // Copy example files
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        URL sampleFileLocation = cl.getResource("Sample_Files" + FileHelper.getSeparator());
+        if (sampleFileLocation != null) {
+            File sampleFiles = null;
             try {
-		@SuppressWarnings("unused")
-		AvailableFiles result =
-			AvailableFiles.getAvailableFilesBean(this.tempDir,
-						this.tempDir + "user_dir"
-						+ this.seperator
-						+ "Sample_Files"
-						+ this.seperator
-						+ "test"
-						+ this.seperator);
-            } catch (Exception e) {
-        	assertTrue(e instanceof IllegalArgumentException);
-
+                sampleFiles = new File(sampleFileLocation.toURI());
+            } catch (URISyntaxException e) {
+                assertTrue("Exception encountered: " + e.getMessage(), false);
             }
-	}
+            FileHelper.copyFileToFile(sampleFiles, this.tempDir + this.seperator);
+            FileHelper.copyFileToFile(sampleFiles, this.tempDir + "user_dir" + this.seperator);
+        } else {
+            assertTrue("Sample files could not be loaded for test", false);
+        }
+    }
 
+    @After
+    public void tearDown() throws Exception {
+        FileUtils.deleteDirectory((new File(this.tempDir)));
+    }
+
+    @Test
+    public void testGetAvailableFilesBeanWithEmptyStringDirectory() {
+        AvailableFiles result =
+                AvailableFiles.getAvailableFilesBean("", "");
+
+        assertNull(result);
+    }
+
+    @Test
+    public void testGetAvailableFilesBeanWithNullDirectory() {
+        AvailableFiles result =
+                AvailableFiles.getAvailableFilesBean(null, null);
+
+        assertNull(result);
+    }
+
+    @Test
+    public void testGetAvailableFilesBeanWithPopulatedExamplesDirectoryNoUserDirectory() {
+        AvailableFiles result =
+                AvailableFiles.getAvailableFilesBean(this.tempDir);
+
+        assertNotNull(result);
+        assertNotNull(result.getExampleFileList());
+        assertFalse(result.getExampleFileList().isEmpty());
+        assertNotNull(result.getExampleFileList());
+        assertFalse(result.getShapeSetList().isEmpty());
+        assertTrue(result.getUserFileList().isEmpty());
+    }
+
+    @Test
+    public void testGetAvailableFilesBeanWithPopulatedExamplesDirectory() {
+        AvailableFiles result =
+                AvailableFiles.getAvailableFilesBean(this.tempDir, null);
+
+        assertNotNull(result);
+        assertNotNull(result.getExampleFileList());
+        assertFalse(result.getExampleFileList().isEmpty());
+        assertNotNull(result.getExampleFileList());
+        assertFalse(result.getShapeSetList().isEmpty());
+        assertTrue(result.getUserFileList().isEmpty());
+    }
+
+    @Test
+    public void testGetAvailableFilesBeanWithPopulatedExamplesDirectoryAndQuotesForUserDir() {
+        AvailableFiles result =
+                AvailableFiles.getAvailableFilesBean(this.tempDir, "");
+
+        assertNotNull(result);
+        assertNotNull(result.getExampleFileList());
+        assertFalse(result.getExampleFileList().isEmpty());
+        assertNotNull(result.getExampleFileList());
+        assertFalse(result.getShapeSetList().isEmpty());
+        assertTrue(result.getUserFileList().isEmpty());
+    }
+
+    @Test
+    public void testSetShapesetListWithPopulatedExamplesDirectory() {
+        AvailableFiles result =
+                AvailableFiles.getAvailableFilesBean(this.tempDir);
+
+        result.setShapeSetList(null);
+        assertEquals(result.getShapeSetList().size(), 0);
+    }
+
+    @Test
+    public void testSetExampleFileList() {
+        AvailableFiles result = new AvailableFiles();
+        List<Files> fileList = new ArrayList<Files>();
+        fileList.add(new Files());
+
+        result.setExampleFileList(fileList);
+        assertNotNull(result.getExampleFileList());
+        assertFalse(result.getExampleFileList().isEmpty());
+    }
+
+    @Test
+    public void testSetUserFileList() {
+        AvailableFiles result = new AvailableFiles();
+        List<Files> fileList = new ArrayList<Files>();
+        fileList.add(new Files());
+
+        result.setUserFileList(fileList);
+        assertNotNull(result.getUserFileList());
+        assertFalse(result.getUserFileList().isEmpty());
+    }
+
+    @Test
+    public void testGetAvailableFilesBeanWithPopulatedUserDirectory() {
+        AvailableFiles result =
+                AvailableFiles.getAvailableFilesBean(this.tempDir,
+                this.tempDir + "user_dir"
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator);
+        assertNotNull(result);
+        assertNotNull(result.getExampleFileList());
+        assertFalse(result.getExampleFileList().isEmpty());
+        assertNotNull(result.getExampleFileList());
+        assertFalse(result.getShapeSetList().isEmpty());
+        assertFalse(result.getUserFileList().isEmpty());
+
+
+    }
+
+    @Test
+    public void testGetAvailableFilesBeanWithBogusUserDirectory() {
+        try {
+            @SuppressWarnings("unused")
+            AvailableFiles result =
+                    AvailableFiles.getAvailableFilesBean(this.tempDir,
+                    this.tempDir + "user_dir"
+                    + this.seperator
+                    + "Sample_Files"
+                    + this.seperator
+                    + "test"
+                    + this.seperator);
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalArgumentException);
+
+        }
+    }
 //	@Test
 //	public void testGetXmlWithEverythingPopulated() {
 //		AvailableFiles filesBean =
