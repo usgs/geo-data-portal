7cb5b71488732b03fdf406d5fb23075277b8a08f
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2010-09-02 20:58:14 +0000

    EOD
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@11167 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- gdp-common-utilities/src/test/java/gov/usgs/cida/gdp/utilities/bean/ShapeFileSetTest.java
+++ gdp-common-utilities/src/test/java/gov/usgs/cida/gdp/utilities/bean/ShapeFileSetTest.java
@@ -21,9 +21,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ShapeFileSetTest {
-	
-	private static final String testFile = "demo_HUCs";
-	
+
+    private static final String testFile = "demo_HUCs";
     private static org.slf4j.Logger log = LoggerFactory.getLogger(ShapeFileSetTest.class);
 
     @BeforeClass
@@ -35,7 +34,6 @@ public class ShapeFileSetTest {
     public static void tearDownAfterClass() throws Exception {
         log.debug("Ended testing class");
     }
-    
     private String tempDir = "";
     private String seperator = "";
 
@@ -82,7 +80,22 @@ public class ShapeFileSetTest {
     }
 
     @Test
-    public void testGetAttributeListFromBean() {
+    public void testSetGetChosenDataset() {
+        ShapeFileSet sfsb = new ShapeFileSet();
+        sfsb.setChosenDataset("test");
+        assertNotNull(sfsb.getChosenDataset());
+        assertEquals(sfsb.getChosenDataset(), "test");
+    }
+
+    @Test
+    public void testGetSerialversionUID() {
+        ShapeFileSet sfsb = new ShapeFileSet();
+        long uid = sfsb.getSerialversionuid();
+        assertTrue(uid != 0);
+    }
+
+    @Test
+    public void testGetChosenFeature() {
         String shpFile = this.tempDir
                 + this.seperator
                 + "Sample_Files"
@@ -114,18 +127,123 @@ public class ShapeFileSetTest {
         files.add(new File(dbfFile));
         filesBean.setFiles(files);
         ShapeFileSet shapeFileSetBean = filesBean.getShapeFileSetBean();
-        assertNotNull(shapeFileSetBean);
-        List<String> result = null;
-        try {
-            result = ShapeFileSet.getAttributeListFromBean(shapeFileSetBean);
-        } catch (IOException e) {
-            assertNotNull(result);
-        }
-
+        shapeFileSetBean.setChosenFeature("test");
+        String result = shapeFileSetBean.getChosenFeature();
         assertNotNull(result);
-        if (result != null) {
-            assertFalse(result.isEmpty());
-        }
+        assertFalse(result.isEmpty());
+        assertEquals(result, "test");
+    }
+
+    @Test
+    public void testGetFeatureListWhenNull() {
+        ShapeFileSet sfs = new ShapeFileSet();
+        sfs.setFeatureList(null);
+        List<String> result = sfs.getFeatureList();
+        assertNotNull(result);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void testGetFeatureListWhenNotNull() {
+        ShapeFileSet sfs = new ShapeFileSet();
+        List<String> input = new ArrayList<String>();
+        input.add("test");
+        sfs.setFeatureList(input);
+        List<String> result = sfs.getFeatureList();
+        assertNotNull(result);
+        assertEquals(result.get(0), "test");
+    }
+
+    @Test
+    public void testGetAttributeListWhenNull() {
+        ShapeFileSet sfs = new ShapeFileSet();
+        sfs.setAttributeList(null);
+        List<String> result = sfs.getAttributeList();
+        assertNotNull(result);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void testGetAttributeListWhenNotNull() {
+        ShapeFileSet sfs = new ShapeFileSet();
+        List<String> input = new ArrayList<String>();
+        input.add("test");
+        sfs.setAttributeList(input);
+        List<String> result = sfs.getAttributeList();
+        assertNotNull(result);
+        assertEquals(result.get(0), "test");
+    }
+
+    @Test
+    public void testGetDBFileFromBean() {
+        String shpFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".shp";
+
+        String prjFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".prj";
+
+        String dbfFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".dbf";
+
+        Files filesBean = new Files();
+        Collection<File> files = new ArrayList<File>();
+        files.add(new File(shpFile));
+        files.add(new File(prjFile));
+        files.add(new File(dbfFile));
+        filesBean.setFiles(files);
+        ShapeFileSet shapeFileSetBean = filesBean.getShapeFileSetBean();
+        assertEquals(shapeFileSetBean.getDbfFile(), new File(dbfFile));
+    }
+
+     @Test
+    public void testGetProjectionFileFromBean() {
+        String shpFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".shp";
+
+        String prjFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".prj";
+
+        String dbfFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".dbf";
+
+        Files filesBean = new Files();
+        Collection<File> files = new ArrayList<File>();
+        files.add(new File(shpFile));
+        files.add(new File(prjFile));
+        files.add(new File(dbfFile));
+        filesBean.setFiles(files);
+        ShapeFileSet shapeFileSetBean = filesBean.getShapeFileSetBean();
+        assertEquals(shapeFileSetBean.getProjectionFile(), new File(prjFile));
     }
 
     @Test
@@ -172,6 +290,7 @@ public class ShapeFileSetTest {
         }
     }
 
+    
     @Test
     public void testGetShapeFileSetBeanFromFilesBean() {
         String shpFile = this.tempDir
@@ -221,4 +340,167 @@ public class ShapeFileSetTest {
         result = ShapeFileSet.getShapeFileSetBeanFromFilesBean(filesBean);
         assertNotNull(result);
     }
+
+    @Test
+    public void testGetShapeFileSetBeanFromFilesBeanWithAllFieldsNull() {
+        ShapeFileSet result = null;
+        Files filesBean = new Files();
+        filesBean.setFiles(new ArrayList<File>());
+        result = ShapeFileSet.getShapeFileSetBeanFromFilesBean(filesBean);
+        assertNull(result);
+    }
+
+    @Test
+    public void testGetShapeFileSetBeanFromFilesBeanListWithNullFilesBeanList() {
+        ShapeFileSet sfs = ShapeFileSet.getShapeFileSetBeanFromFilesBeanList(new ArrayList<Files>(), testFile);
+        assertNull(sfs);
+    }
+
+        @Test
+    public void testGetShapeFileSetBeanFromFilesBeanList() {
+            String shpFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".shp";
+
+        String prjFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".prj";
+
+        String dbfFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".dbf";
+
+        String shxFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".shx";
+
+        ShapeFileSet result = null;
+        Files filesBean = new Files();
+        Collection<File> files = new ArrayList<File>();
+        
+        files.add(new File(shpFile));
+        files.add(new File(prjFile));
+        files.add(new File(dbfFile));
+        files.add(new File(shxFile));
+        filesBean.setFiles(files);
+        List<Files> input = new ArrayList<Files>();
+        filesBean.setName(testFile + ".dbf");
+        input.add(filesBean);
+        result = ShapeFileSet.getShapeFileSetBeanFromFilesBeanList(input, testFile + ".dbf");
+        assertNotNull(result);
+    }
+
+          @Test
+    public void testGetShapeFileSetBeanFromFilesBeanListWithWrongFilesBeanName() {
+            String shpFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".shp";
+
+        String prjFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".prj";
+
+        String dbfFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".dbf";
+
+        String shxFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".shx";
+
+        ShapeFileSet result = null;
+        Files filesBean = new Files();
+        Collection<File> files = new ArrayList<File>();
+
+        files.add(new File(shpFile));
+        files.add(new File(prjFile));
+        files.add(new File(dbfFile));
+        files.add(new File(shxFile));
+        filesBean.setFiles(files);
+        List<Files> input = new ArrayList<Files>();
+        filesBean.setName("wrong");
+        input.add(filesBean);
+        result = ShapeFileSet.getShapeFileSetBeanFromFilesBeanList(input, testFile + ".dbf");
+        assertNull(result);
+    }
+
+     @Test
+    public void testGetShapeFileIndexSetBeanFromFilesBean() {
+        String shpFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".shp";
+
+        String prjFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".prj";
+
+        String dbfFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".dbf";
+
+        String shxFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".shx";
+
+        ShapeFileSet result = null;
+        Files filesBean = new Files();
+        Collection<File> files = new ArrayList<File>();
+        files.add(new File(shpFile));
+        files.add(new File(prjFile));
+        files.add(new File(dbfFile));
+        files.add(new File(shxFile));
+        filesBean.setFiles(files);
+        result = ShapeFileSet.getShapeFileSetBeanFromFilesBean(filesBean);
+        assertNotNull(result);
+        assertEquals(result.getShapeFileIndexFile(),new File(shxFile));
+    }
+
 }
