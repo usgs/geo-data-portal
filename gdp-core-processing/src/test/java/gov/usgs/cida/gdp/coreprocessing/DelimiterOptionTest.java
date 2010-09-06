3d19675af655132d6e60b23e532fb76c6eeb0f2d
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2010-09-06 03:40:27 +0000

    DelimiterOption 100% covered
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@11174 622dc1fb-2224-0410-ab72-dc85a3975e1a

---
+++ gdp-core-processing/src/test/java/gov/usgs/cida/gdp/coreprocessing/DelimiterOptionTest.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.usgs.cida.gdp.coreprocessing;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author isuftin
+ */
+public class DelimiterOptionTest {
+
+    public DelimiterOptionTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of values method, of class DelimiterOption.
+     */
+    @Test
+    public void testValues() {
+        assertEquals(DelimiterOption.c.toString(), "[comma]");
+        assertEquals(DelimiterOption.s.toString(), "[space]");
+        assertEquals(DelimiterOption.t.toString(), "[tab]");
+        assertEquals(DelimiterOption.getDefault(), DelimiterOption.c);
+    }
+
+    /**
+     * Test of valueOf method, of class DelimiterOption.
+     */
+    @Test
+    public void testValueOf() {
+    }
+
+    /**
+     * Test of getDefault method, of class DelimiterOption.
+     */
+    @Test
+    public void testGetDefault() {
+    }
+
+    /**
+     * Test of toString method, of class DelimiterOption.
+     */
+    @Test
+    public void testToString() {
+    }
+
+}
