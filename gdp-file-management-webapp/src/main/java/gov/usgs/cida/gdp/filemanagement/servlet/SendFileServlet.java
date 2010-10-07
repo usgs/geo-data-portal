2b2a55801a2b3579665fa444f549e1cd1d679af7
jiwalker <jiwalker@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2010-10-07 15:10:12 +0000

    Zipped output works, must set zipped=yes in the request.
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@11575 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- gdp-file-management-webapp/src/main/java/gov/usgs/cida/gdp/filemanagement/servlet/SendFileServlet.java
+++ gdp-file-management-webapp/src/main/java/gov/usgs/cida/gdp/filemanagement/servlet/SendFileServlet.java
@@ -6,10 +6,14 @@ import gov.usgs.cida.gdp.utilities.XmlUtils;
 import gov.usgs.cida.gdp.utilities.bean.Acknowledgement;
 import gov.usgs.cida.gdp.utilities.bean.Error;
 import gov.usgs.cida.gdp.utilities.bean.XmlReply;
+import java.io.OutputStream;
 import java.io.BufferedInputStream;
+//import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 import java.util.Date;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -44,68 +48,94 @@ public class SendFileServlet extends HttpServlet {
 
         Long start = Long.valueOf(new Date().getTime());
 
-		String command = request.getParameter("command");
+        String command = request.getParameter("command");
 
         // User wishes to grab a file. Send this file if available.
-		if ("getfile".equals(command)) {
-			String file = request.getParameter("file");
-			String baseFilePath = System.getProperty("applicationTempDir");
-	    	baseFilePath = baseFilePath + FileHelper.getSeparator();
-	    	String fullFilePath = baseFilePath + "upload-repository" +FileHelper.getSeparator()+file;
-	    	File fileToUpload = null;
+        if ("getfile".equals(command)) {
+            String file = request.getParameter("file");
+            boolean zipped = ("yes".equals(request.getParameter("zipped")));
+            String baseFilePath = System.getProperty("applicationTempDir");
+            baseFilePath = baseFilePath + FileHelper.getSeparator();
+            String fullFilePath = baseFilePath + "upload-repository" + FileHelper.getSeparator() + file;
+            File fileToUpload = null;
 
-	    	if (!FileHelper.doesDirectoryOrFileExist(fullFilePath)) {
-				XmlReply xmlOutput = new XmlReply(Acknowledgement.ACK_FAIL, new Error(Error.ERR_FILE_NOT_FOUND));
-				XmlUtils.sendXml(xmlOutput, start, response);
-				return;
-	    	}
-	    	fileToUpload = new File(fullFilePath);
+            if (!FileHelper.doesDirectoryOrFileExist(fullFilePath)) {
+                XmlReply xmlOutput = new XmlReply(Acknowledgement.ACK_FAIL, new Error(Error.ERR_FILE_NOT_FOUND));
+                XmlUtils.sendXml(xmlOutput, start, response);
+                return;
+            }
+            fileToUpload = new File(fullFilePath);
 
-	    	// Set the headers.
-	    	response.setContentType("application/x-download");
-	    	response.setHeader("Content-Disposition", "attachment; filename=" + fileToUpload.getName());
-	    	response.setCharacterEncoding("UTF-8");
+            // Set the headers.
+            if (zipped) {
+                response.setContentType("application/zip");
+                response.addHeader("Content-transfer-encoding", "binary");
+                response.setHeader("Content-Disposition", "attachment; filename=" + "gdp_output.zip");
+            }
+            else {
+                response.setContentType("application/x-download");
+                response.setHeader("Content-Disposition", "attachment; filename=" + fileToUpload.getName());
+                response.setCharacterEncoding("UTF-8");
+            }
 
-	    	// Send the file.
-	    	ServletOutputStream out = null;
-	    	BufferedInputStream buf = null;
-	    	try {
-		    	out = response.getOutputStream();
-		    	response.setContentLength((int) fileToUpload.length());
-		    	FileInputStream input = new FileInputStream(fileToUpload);
-		    	buf = new BufferedInputStream(input);
-		    	int readBytes = 0;
-		    	while ((readBytes = buf.read()) != -1) out.write(readBytes);
-		    	out.close();
-		    	buf.close();
-	    	} catch (IOException ioe) {
-	    	      throw new ServletException(ioe.getMessage());
-	    	} finally {
-	    		if (out != null)
-	    			out.close();
-	    	      if (buf != null)
-	    	    	  buf.close();
-	    	}
+            // Send the file.
+            OutputStream out = null;
+            BufferedInputStream buf = null;
+            ZipOutputStream zipOut = null;
+            try {
+                if (zipped) {
+                    zipOut = new ZipOutputStream(response.getOutputStream());
+                    zipOut.putNextEntry(new ZipEntry(fileToUpload.getName()));
+                    out = zipOut;
+                }
+                else {
+                    out = response.getOutputStream();
+                }
+                response.setContentLength((int) fileToUpload.length());
+                FileInputStream input = new FileInputStream(fileToUpload);
+                buf = new BufferedInputStream(input);
+                int readBytes = 0;
+                while ((readBytes = buf.read()) != -1) {
+                    out.write(readBytes);
+                }
+                if (zipped) {
+                    zipOut.closeEntry();
+                }
+                out.close();
+                buf.close();
+            } catch (IOException ioe) {
+                throw new ServletException(ioe.getMessage());
+            } finally {
+                if (zipOut != null) {
+                    zipOut.closeEntry();
+                }
+                if (out != null) {
+                    out.close();
+                }
+                if (buf != null) {
+                    buf.close();
+                }
+            }
             return;
-		}
+        }
 
         // Checks the upload repository for finished process file availability
         if ("checkuploadfile".equals(command)) {
-			String file = request.getParameter("file");
-			String baseFilePath = System.getProperty("applicationTempDir");
-	    	baseFilePath = baseFilePath + FileHelper.getSeparator();
-	    	String fullFilePath = baseFilePath + "upload-repository" +FileHelper.getSeparator()+file;
-	    	boolean fileExists = FileHelper.doesDirectoryOrFileExist(fullFilePath);
-	    	boolean hasBytes = false;
-	    	File tempFile = new File(fullFilePath);
-	    	hasBytes = tempFile.length() > 0;
-	    	boolean fileExistsAndHasBytes = fileExists & hasBytes;
-	    	UploadFileCheck ufcb = new UploadFileCheck(file, fileExistsAndHasBytes);
+            String file = request.getParameter("file");
+            String baseFilePath = System.getProperty("applicationTempDir");
+            baseFilePath = baseFilePath + FileHelper.getSeparator();
+            String fullFilePath = baseFilePath + "upload-repository" + FileHelper.getSeparator() + file;
+            boolean fileExists = FileHelper.doesDirectoryOrFileExist(fullFilePath);
+            boolean hasBytes = false;
+            File tempFile = new File(fullFilePath);
+            hasBytes = tempFile.length() > 0;
+            boolean fileExistsAndHasBytes = fileExists & hasBytes;
+            UploadFileCheck ufcb = new UploadFileCheck(file, fileExistsAndHasBytes);
 
-			XmlReply xmlReply = new XmlReply(Acknowledgement.ACK_OK, ufcb);
-			XmlUtils.sendXml(xmlReply, start, response);
-			return;
-		}
+            XmlReply xmlReply = new XmlReply(Acknowledgement.ACK_OK, ufcb);
+            XmlUtils.sendXml(xmlReply, start, response);
+            return;
+        }
 
         //TODO- Do not know why this code is here. Check to see if it's used
         BufferedInputStream buf = null;
@@ -136,7 +166,7 @@ public class SendFileServlet extends HttpServlet {
             }
         }
 
-        
+
     }
 
     /**
