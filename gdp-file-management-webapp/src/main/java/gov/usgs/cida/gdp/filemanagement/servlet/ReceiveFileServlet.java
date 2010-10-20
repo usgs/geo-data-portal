31a1e7a7a8c441e5d7f6820f735273c693f0a2aa
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2010-10-20 13:19:40 +0000

    -- Getting closer to successfully uploading files from front-end to WFS and back
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@11662 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- gdp-file-management-webapp/src/main/java/gov/usgs/cida/gdp/filemanagement/servlet/ReceiveFileServlet.java
+++ gdp-file-management-webapp/src/main/java/gov/usgs/cida/gdp/filemanagement/servlet/ReceiveFileServlet.java
@@ -1,17 +1,16 @@
 package gov.usgs.cida.gdp.filemanagement.servlet;
 
 import gov.usgs.cida.gdp.utilities.FileHelper;
-import gov.usgs.cida.gdp.utilities.XmlUtils;
-import gov.usgs.cida.gdp.utilities.bean.Acknowledgement;
-import gov.usgs.cida.gdp.utilities.bean.Error;
-import gov.usgs.cida.gdp.utilities.bean.UserDirectory;
 import gov.usgs.cida.gdp.utilities.bean.XmlReply;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 import java.util.*;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
@@ -19,6 +18,7 @@ import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemFactory;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.LoggerFactory;
 
 /**
@@ -30,6 +30,7 @@ public class ReceiveFileServlet extends HttpServlet {
 
     private static final long serialVersionUID = 6766229674722132238L;
     private static org.slf4j.Logger log = LoggerFactory.getLogger(ReceiveFileServlet.class);
+
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
@@ -44,38 +45,69 @@ public class ReceiveFileServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Long start = Long.valueOf(new Date().getTime());
-
         XmlReply xmlOutput = null;
-        String command = request.getParameter("command");
+        String tempLocation = ("".equals(System.getProperty("gdp.shapefile.temp.path")) || System.getProperty("shapefile.temp.path") == null) ? System.getProperty("java.io.tmpdir") : System.getProperty("gdp.io.tmpdir");
+        String applicationUserspaceDir = tempLocation + File.separator + "GDP" + File.separator + UUID.randomUUID() + File.separator;
+        FileHelper.createDir(applicationUserspaceDir);
+//            String userDirectory = "";
+//
+//            try {
+//                userDirectory = uploadFiles(request, applicationUserspaceDir);
+//            } catch (Exception e) {
+//                xmlOutput = new XmlReply(Acknowledgement.ACK_FAIL, new Error(Error.ERR_FILE_UPLOAD, e));
+//                XmlUtils.sendXml(xmlOutput, start, response);
+//                return;
+//            }
+//
+//            if ("".equals(userDirectory)) { // User directory could not be created
+//                xmlOutput = new XmlReply(Acknowledgement.ACK_FAIL, new Error(Error.ERR_USER_DIR_CREATE));
+//                XmlUtils.sendXml(xmlOutput, start, response);
+//                return;
+//            }
+//
+//            // Work directly into another webservice to list the files available
+//            // including the file the user just uploaded
+//            log.debug("Files successfully uploaded.");
+//            RequestDispatcher rd = request.getRequestDispatcher("/FileSelectionServlet?command=listfiles&userdirectory=" + userDirectory);
+//            rd.forward(request, response);
+//            return;
+//        }
+        PrintWriter writer = null;
+        InputStream is = null;
+        FileOutputStream fos = null;
 
-        if (ServletFileUpload.isMultipartContent(request)) {
-            // "gdp.shapefile.temp.path" should be set in the tomcat startup script or setenv.sh as JAVA_OPTS="-Dgdp.shapefile.temp.path=/wherever/you/want/this/file/placed"
-            String tempLocation = ("".equals(System.getProperty("gdp.shapefile.temp.path")) || System.getProperty("shapefile.temp.path") == null)? System.getProperty("java.io.tmpdir") : System.getProperty("gdp.io.tmpdir");
-            String applicationUserspaceDir = tempLocation +  File.separator + "GDP" + File.separator +  UUID.randomUUID();
-            
-            String userDirectory = "";
-
-            try {
-                userDirectory = uploadFiles(request, applicationUserspaceDir);
-            } catch (Exception e) {
-                xmlOutput = new XmlReply(Acknowledgement.ACK_FAIL, new Error(Error.ERR_FILE_UPLOAD, e));
-                XmlUtils.sendXml(xmlOutput, start, response);
-                return;
-            }
-
-            if ("".equals(userDirectory)) { // User directory could not be created
-                xmlOutput = new XmlReply(Acknowledgement.ACK_FAIL, new Error(Error.ERR_USER_DIR_CREATE));
-                XmlUtils.sendXml(xmlOutput, start, response);
-                return;
-            }
-
-            // Work directly into another webservice to list the files available
-            // including the file the user just uploaded
-            log.debug("Files successfully uploaded.");
-            RequestDispatcher rd = request.getRequestDispatcher("/FileSelectionServlet?command=listfiles&userdirectory=" + userDirectory);
-            rd.forward(request, response);
-            return;
+        try {
+            writer = response.getWriter();
+        } catch (IOException ex) {
+            log(ReceiveFileServlet.class.getName() + "has thrown an exception: " + ex.getMessage());
         }
+
+        String filename = request.getHeader("X-File-Name");
+        try {
+            is = request.getInputStream();
+            fos = new FileOutputStream(new File(applicationUserspaceDir + filename));
+            IOUtils.copy(is, fos);
+            response.setStatus(response.SC_OK);
+            writer.print("{success: true}");
+        } catch (FileNotFoundException ex) {
+            response.setStatus(response.SC_INTERNAL_SERVER_ERROR);
+            writer.print("{success: false}");
+            log(ReceiveFileServlet.class.getName() + "has thrown an exception: " + ex.getMessage());
+        } catch (IOException ex) {
+            response.setStatus(response.SC_INTERNAL_SERVER_ERROR);
+            writer.print("{success: false}");
+            log(ReceiveFileServlet.class.getName() + "has thrown an exception: " + ex.getMessage());
+        } finally {
+            try {
+                fos.close();
+                is.close();
+            } catch (IOException ignored) {
+            }
+        }
+
+        writer.flush();
+        writer.close();
+
     }
 
     /**
