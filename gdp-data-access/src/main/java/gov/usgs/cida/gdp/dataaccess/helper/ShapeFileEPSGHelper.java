f82c96a599abda0afee8b273ec088a469905349f
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2010-12-30 20:13:34 +0000

    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@12404 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- gdp-data-access/src/main/java/gov/usgs/cida/gdp/dataaccess/helper/ShapeFileEPSGHelper.java
+++ gdp-data-access/src/main/java/gov/usgs/cida/gdp/dataaccess/helper/ShapeFileEPSGHelper.java
@@ -1,6 +1,11 @@
 package gov.usgs.cida.gdp.dataaccess.helper;
 
+import gov.usgs.cida.gdp.utilities.FileHelper;
 import java.io.File;
+import java.io.IOException;
+import org.geotools.referencing.CRS;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,36 +14,58 @@ import org.slf4j.LoggerFactory;
  * @author isuftin
  */
 public class ShapeFileEPSGHelper {
-    Logger log = LoggerFactory.getLogger(ShapeFileEPSGHelper.class);
+    private static Logger log = LoggerFactory.getLogger(ShapeFileEPSGHelper.class);
 
-    private File zippedShapeFile;
+    private File prjFile;
 
-    ShapeFileEPSGHelper() {}
-
-    ShapeFileEPSGHelper(final File zippedShapeFile)  {
-        this.zippedShapeFile = zippedShapeFile;
+    ShapeFileEPSGHelper(final File prjFile)  {
+        this.prjFile = prjFile;
     }
 
-    public File getPRJFileFromZip(final File zippedShapeFile) {
-        File result = null;
+    public String getEpsgFromPrj() {
+        String result = null;
+        return result;
+    }
 
-        
+    public static String getEpsgFromPrj(final File prjFile) throws IOException, FactoryException {
+        String result = null;
+        if (prjFile == null || !prjFile.exists()) return result;
+
+        byte[] wktByteArray = FileHelper.getByteArrayFromFile(prjFile);
+        result = ShapeFileEPSGHelper.getEpsgFromWkt(new String(wktByteArray));
+
+        return result;
+    }
+
+    public static String getEpsgFromWkt(final String wkt) throws FactoryException {
+        String result = null;
+        if (wkt == null || "".equals(wkt)) return result;
+
+        CoordinateReferenceSystem crs = null;
+        try {
+            crs = CRS.parseWKT(wkt);
+        } catch (FactoryException ex) {
+            log.error(ex.getMessage());
+            throw ex;
+        }
+
+        result = CRS.lookupIdentifier(crs, true);
 
         return result;
     }
 
     /**
-     * @return the zippedShapeFile
+     * @return the prjFile
      */
-    public File getZippedShapeFile() {
-        return zippedShapeFile;
+    public File getPrjFile() {
+        return this.prjFile;
     }
 
     /**
-     * @param zippedShapeFile the zippedShapeFile to set
+     * @param prjFile the prjFile to set
      */
-    public void setZippedShapeFile(File zippedShapeFile) {
-        this.zippedShapeFile = zippedShapeFile;
+    public void setPrjFile(File prjFile) {
+        this.prjFile = prjFile;
     }
 
 }
--- gdp-data-access/src/test/java/gov/usgs/cida/gdp/dataaccess/helper/ShapeFileEPSGHelperTest.java
+++ gdp-data-access/src/test/java/gov/usgs/cida/gdp/dataaccess/helper/ShapeFileEPSGHelperTest.java
@@ -5,19 +5,40 @@
 
 package gov.usgs.cida.gdp.dataaccess.helper;
 
+import gov.usgs.cida.gdp.utilities.FileHelper;
+import gov.usgs.cida.gdp.utilities.bean.Files;
+import gov.usgs.cida.gdp.utilities.bean.ShapeFileSet;
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.opengis.referencing.FactoryException;
 
 /**
  *
  * @author isuftin
  */
 public class ShapeFileEPSGHelperTest {
+
+    private File prjFile = null;
+    private String tempDir = "";
+    private String seperator = "";
+    private String testFile = "demo_HUCs";
+    private List<ShapeFileSet> shapeBeanList = new ArrayList<ShapeFileSet>();
+    private String wkt = "PROJCS[\"NAD_1983_Albers\",GEOGCS[\"GCS_North_American_1983\",DATUM[\"D_North_American_1983\",SPHEROID[\"GRS_1980\",6378137.0,298.257222101]],PRIMEM[\"Greenwich\",0.0],UNIT[\"Degree\",0.0174532925199433]],PROJECTION[\"Albers\"],PARAMETER[\"False_Easting\",0.0],PARAMETER[\"False_Northing\",0.0],PARAMETER[\"Central_Meridian\",-96.0],PARAMETER[\"Standard_Parallel_1\",29.5],PARAMETER[\"Standard_Parallel_2\",45.5],PARAMETER[\"Latitude_Of_Origin\",23.0],UNIT[\"Meter\",1.0]]";
+
 
     public ShapeFileEPSGHelperTest() {
     }
@@ -30,37 +51,117 @@ public class ShapeFileEPSGHelperTest {
     public static void tearDownClass() throws Exception {
     }
 
-    @Before
+ @Before
     public void setUp() {
+        this.tempDir = System.getProperty("java.io.tmpdir");
+
+        if (!(this.tempDir.endsWith("/") || this.tempDir.endsWith("\\"))) {
+            this.tempDir = this.tempDir + System.getProperty("file.separator");
+        }
+        String systemTempDir = System.getProperty("java.io.tmpdir");
+        this.seperator = java.io.File.separator;
+        String currentTime = Long.toString((new Date()).getTime());
+        this.tempDir = systemTempDir + this.seperator + currentTime;
+        (new File(this.tempDir)).mkdir();
+
+        // Copy example files
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        URL sampleFileLocation = cl.getResource("Sample_Files/");
+        if (sampleFileLocation != null) {
+            File sampleFiles = null;
+            try {
+                sampleFiles = new File(sampleFileLocation.toURI());
+            } catch (URISyntaxException e) {
+                assertTrue("Exception encountered: " + e.getMessage(), false);
+            }
+            try {
+                FileHelper.copyFileToFile(sampleFiles, this.tempDir + this.seperator);
+            } catch (IOException ex) {
+                Logger.getLogger(ShapeFileEPSGHelperTest.class.getName()).log(Level.SEVERE, null, ex);
+                fail(ex.getMessage());
+            }
+        } else {
+            assertTrue("Sample files could not be loaded for test", false);
+        }
+        String prjFile = this.tempDir
+                + this.seperator
+                + "Sample_Files"
+                + this.seperator
+                + "Shapefiles"
+                + this.seperator
+                + testFile + ".prj";
+
+        this.prjFile = new File(prjFile);
+
     }
 
     @After
     public void tearDown() {
+        File delDir = new File(this.tempDir);
+        try {
+            FileHelper.deleteDirRecursively(delDir);
+        } catch (IOException ex) {
+            Logger.getLogger(ShapeFileEPSGHelperTest.class.getName()).log(Level.SEVERE, "Failed to delete: " + delDir.getPath() + "  -- Remember to clean project or remove this file/dir.", ex);
+        }
     }
 
     /**
      * Test of getShapeFile method, of class ShapeFileEPSGHelper.
      */
     @Test
-    public void testGetShapeFile() {
+    public void testGetShapeFileDirectory() {
         System.out.println("getShapeFile");
-        ShapeFileEPSGHelper instance = new ShapeFileEPSGHelper();
         File expResult = new File(System.getProperty("java.io.tmpdir"));
-        instance.setZippedShapeFile(expResult);
-        File result = instance.getZippedShapeFile();
+        ShapeFileEPSGHelper instance = new ShapeFileEPSGHelper(expResult);
+        instance.setPrjFile(expResult);
+        File result = instance.getPrjFile();
         assertEquals(expResult, result);
     }
 
     /**
-     * Test of setZippedShapeFile method, of class ShapeFileEPSGHelper.
+     * Test of setPrjFile method, of class ShapeFileEPSGHelper.
      */
     @Test
-    public void testSetShapeFile() {
+    public void testSetShapeFileDirectory() {
         System.out.println("setShapeFile");
-        ShapeFileEPSGHelper instance = new ShapeFileEPSGHelper();
         File expResult = new File(System.getProperty("java.io.tmpdir"));
-        instance.setZippedShapeFile(expResult);
-        File result = instance.getZippedShapeFile();
+        ShapeFileEPSGHelper instance = new ShapeFileEPSGHelper(expResult);
+        instance.setPrjFile(expResult);
+        File result = instance.getPrjFile();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setPrjFile method, of class ShapeFileEPSGHelper.
+     */
+    @Test
+    public void testGetEpsgFromPrj() {
+        System.out.println("getEpsgFromPrj");
+        String result = null;
+        try {
+            result = ShapeFileEPSGHelper.getEpsgFromPrj(this.prjFile);
+        } catch (IOException ex) {
+            Logger.getLogger(ShapeFileEPSGHelperTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (FactoryException ex) {
+            Logger.getLogger(ShapeFileEPSGHelperTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        String expResult = "EPSG:4269";
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setPrjFile method, of class ShapeFileEPSGHelper.
+     */
+    @Test
+    public void testGetEpsgFromWkt() {
+        System.out.println("getEpsgFromWkt");
+        String result = null;
+        try {
+            result = ShapeFileEPSGHelper.getEpsgFromWkt(wkt);
+        } catch (FactoryException ex) {
+            Logger.getLogger(ShapeFileEPSGHelperTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        String expResult = "EPSG:5070";
         assertEquals(expResult, result);
     }
 
