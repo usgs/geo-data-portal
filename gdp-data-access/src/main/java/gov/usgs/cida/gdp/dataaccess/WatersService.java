cbfeac4eb1a36f41ceab5f1514cd68bcdd280df2
razoerb <razoerb@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-02-17 22:43:33 +0000

    Fix parsing failure when WATERS returns a single polygon.
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@13341 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- gdp-data-access/src/main/java/gov/usgs/cida/gdp/dataaccess/WatersService.java
+++ gdp-data-access/src/main/java/gov/usgs/cida/gdp/dataaccess/WatersService.java
@@ -12,8 +12,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringReader;
+import java.io.StringWriter;
 import java.net.URL;
 import javax.xml.parsers.ParserConfigurationException;
+import org.apache.xerces.parsers.DOMParser;
 import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.JsonParser;
@@ -22,6 +24,13 @@ import org.geotools.data.shapefile.dbf.DbaseFileWriter;
 import org.geotools.data.shapefile.shp.ShapeType;
 import org.geotools.data.shapefile.shp.ShapefileWriter;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSOutput;
+import org.w3c.dom.ls.LSSerializer;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 /**
@@ -30,6 +39,12 @@ import org.xml.sax.SAXException;
 public class WatersService {
 
     static org.slf4j.Logger log = LoggerFactory.getLogger(WatersService.class);
+
+    static final String multiPolyWrap =
+        "<gml:MultiPolygon xmlns:gml=\"http://www.opengis.net/gml\" srsName=\"SDO:8265\">" +
+          "<gml:polygonMember>" +
+          "</gml:polygonMember>" +
+        "</gml:MultiPolygon>";
 
     public static File getGeometry(String lon, String lat, String name) throws Exception {
         
@@ -52,7 +67,7 @@ public class WatersService {
                 + "&pMaxDistance=999999999"), "GET");
 
         String gml = parseJSON(json, "shape");
-        GeometryCollection coll = parseGML(gml);
+        Geometry geom = parseGML(gml);
 
         // Write to a shapefile so GeoServer can load the geometry. As of 2.0.2,
         // Geoserver (GeoTools) GML datastores are unsupported. Hence the
@@ -78,15 +93,15 @@ public class WatersService {
         FileOutputStream shxFileOutputStream = new FileOutputStream(shxFile);
         FileOutputStream dbfFileOutputStream = new FileOutputStream(dbfFile);
 
-        int numFeatures = coll.getNumGeometries();
+        int numFeatures = geom.getNumGeometries();
 
-        // The WATERS service returns one big polygon, along with a couple smaller
-        // polygons (3-4 vertices). We only care about the big polygon, so find
-        // which index it's at in the collection.
+        // The WATERS service returns one big polygon, potentially along with a
+        // couple smaller polygons (3-4 vertices). We only care about the big
+        // polygon, so find which index it's at in the collection.
         int maxNumPoints = -1;
         int maxGeomIndex = -1;
         for (int i = 0; i < numFeatures; i++) {
-            int numPoints = coll.getGeometryN(i).getNumPoints();
+            int numPoints = geom.getGeometryN(i).getNumPoints();
 
             if (numPoints > maxNumPoints) {
                 maxNumPoints = numPoints;
@@ -94,9 +109,9 @@ public class WatersService {
             }
         }
 
-        Geometry bigPoly = coll.getGeometryN(maxGeomIndex);
+        Geometry bigPoly = geom.getGeometryN(maxGeomIndex);
 
-        GeometryFactory factory = coll.getFactory();
+        GeometryFactory factory = geom.getFactory();
         GeometryCollection bigPolyColl =
                 new GeometryCollection(new Geometry[] { bigPoly }, factory);
 
@@ -145,22 +160,67 @@ public class WatersService {
         return null;
     }
 
-    private static GeometryCollection parseGML(String gml)
+    private static Geometry parseGML(String gml)
             throws IOException, SAXException, ParserConfigurationException {
 
         if (gml == null) {
             throw new IllegalArgumentException();
         }
 
+        gml = convertToMultiPolygon(gml);
+
         //create the parser with the gml 2.0 configuration
         org.geotools.xml.Configuration configuration = new org.geotools.gml2.GMLConfiguration();
         org.geotools.xml.Parser parser = new org.geotools.xml.Parser(configuration);
 
-
-        // TODO: parse fails with when gml is only a single polygon with "Authority "SDO" is unknown".
-        //parse
-        GeometryCollection geom = (GeometryCollection) parser.parse(new StringReader(gml));
+        Geometry geom = (Geometry) parser.parse(new StringReader(gml));
 
         return geom;
+    }
+
+    // This is a hack to get around (what appears to be) a GeoTools GML parsing
+    // bug. If the GML has a MultiPolygon as the root element, GeoTools has no
+    // problem parsing it. However, if the root element is a Polygon, parsing
+    // fails. This method wraps a bare Polygon with the multiPolyWrap XML stub.
+    // NOTE: this is dependent on the format of the WATERS response, and could
+    // break if that format changes.
+    static String convertToMultiPolygon(String gml) throws IOException, SAXException {
+        DOMParser gmlParser = new DOMParser();
+
+        gmlParser.parse(new InputSource(new StringReader(gml)));
+
+        Document gmlDoc = gmlParser.getDocument();
+        Node root = gmlDoc.getFirstChild();
+
+        if (root.getNodeName().equals("gml:Polygon")) {
+            DOMParser wrapParser = new DOMParser();
+            wrapParser.parse(new InputSource(new StringReader(multiPolyWrap)));
+            Document wrapDoc = wrapParser.getDocument();
+
+            // Remove attributes of Polygon (will be attrs of parent MultiPolygon instead)
+            NamedNodeMap attrs = root.getAttributes();
+            attrs.removeNamedItem("xmlns:gml");
+            attrs.removeNamedItem("srsName");
+
+            Node polyNode = wrapDoc.importNode(root, true);
+            Node polyMembersNode = wrapDoc.getFirstChild().getFirstChild();
+            polyMembersNode.appendChild(polyNode);
+
+
+            DOMImplementationLS domLS =(DOMImplementationLS)(wrapDoc.getImplementation()).
+                   getFeature("LS","3.0");
+
+            StringWriter sw = new StringWriter();
+
+            LSOutput lsOut = domLS.createLSOutput();
+            lsOut.setCharacterStream(sw);
+
+            LSSerializer serializer = domLS.createLSSerializer();
+            serializer.write(wrapDoc, lsOut);
+
+            return sw.toString();
+        } else {
+            return gml;
+        }
     }
 }
