f05e61f14864db26f1352311c43aacd5ced63bf8
tkunicki <tkunicki@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-02-23 19:17:34 +0000

    BinZippedBase64SHPParser now streams requests instead of operating in-heap (more!).
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@13418 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- gdp-utility-wps/src/main/java/gov/usgs/cida/gdp/wps/parser/BinZippedBase64SHPParser.java
+++ gdp-utility-wps/src/main/java/gov/usgs/cida/gdp/wps/parser/BinZippedBase64SHPParser.java
@@ -1,10 +1,7 @@
 package gov.usgs.cida.gdp.wps.parser;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import org.n52.wps.io.IOUtils;
+import org.apache.commons.codec.binary.Base64InputStream;
 import org.n52.wps.io.data.GenericFileData;
 import org.n52.wps.io.data.IData;
 import org.n52.wps.io.data.binding.complex.GenericFileDataBinding;
@@ -20,19 +17,10 @@ public class BinZippedBase64SHPParser extends AbstractBinZippedSHPParser {
     }
 
     /**
-     * @throws RuntimeException
-     *             if an error occurs while writing the stream to disk or
-     *             unzipping the written file
      * @see org.n52.wps.io.IParser#parse(java.io.InputStream)
      */
     @Override
-    public IData parse(InputStream input, String mimeType) throws RuntimeException {
-        try {
-            File zipped = IOUtils.writeBase64ToFile(input, "zip");
-            return new GenericFileDataBinding(new GenericFileData(new FileInputStream(zipped), mimeType));
-        } catch (IOException e) {
-            throw new RuntimeException(
-                    "An error has occurred while accessing provided data", e);
-        }
+    public IData parse(InputStream input, String mimeType) {
+        return new GenericFileDataBinding(new GenericFileData(new Base64InputStream(input), mimeType));
     }
 }
