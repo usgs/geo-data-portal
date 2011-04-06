f4ba09aa695a3561f8c4b2a2a0c5d40f18fa4d9d
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-04-06 17:22:40 +0000

    Finished the unit tests for the core function in this Issue #GDP-210 - Support shapefile upload zips with files contained in folder
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@14214 622dc1fb-2224-0410-ab72-dc85a3975e1a

---
+++ gdp-utility-wps/src/test/java/gov/usgs/cida/gdp/io/data/ZippedGenericFileDataTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gov.usgs.cida.gdp.io.data;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URL;
+import java.util.UUID;
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author isuftin
+ */
+public class ZippedGenericFileDataTest {
+    private File multiDirZip = null;
+    private FileInputStream fis = null;
+    private File tempArea = null;
+    public ZippedGenericFileDataTest() {
+    }
+
+    @Before
+    public  void beforeTest() throws Exception {
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        URL sampleFileLocation = cl.getResource("Sample_Zips/testShapes_multidir.zip");
+        multiDirZip = new File(sampleFileLocation.toURI());
+        fis = new FileInputStream(multiDirZip);
+        tempArea = new File(System.getProperty("java.io.tmpdir") + File.separator + UUID.randomUUID().toString());
+        FileUtils.forceMkdir(tempArea);
+    }
+
+    @After
+    public  void afterTest() throws Exception {
+        fis.close();
+        FileUtils.deleteQuietly(tempArea);
+    }
+
+    @Test
+    public void testSomeMethod() {
+        ZippedGenericFileData test = new ZippedGenericFileData(fis, "application/x-zipped-shp");
+        test.writeData(tempArea);
+        assertFalse(FileUtils.listFiles(tempArea, null, true).isEmpty());
+        assertTrue(FileUtils.listFiles(tempArea, null, true).size() == 6);
+    }
+}
