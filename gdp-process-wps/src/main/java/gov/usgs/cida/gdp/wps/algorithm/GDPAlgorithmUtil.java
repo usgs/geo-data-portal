b10f71c63c0c06cedfa52e29685061195fd2b103
tkunicki <tkunicki@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-04-12 19:07:34 +0000

    removed deprecated algorithm implementations (cont.)
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@14310 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- gdp-process-wps/src/main/java/gov/usgs/cida/gdp/wps/algorithm/GDPAlgorithmUtil.java
+++ gdp-process-wps/src/main/java/gov/usgs/cida/gdp/wps/algorithm/GDPAlgorithmUtil.java
@@ -4,18 +4,9 @@ import gov.usgs.cida.gdp.wps.util.WCSUtil;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.Formatter;
-import java.util.List;
-import java.util.Map;
-import org.geotools.feature.FeatureCollection;
 import org.geotools.geometry.jts.ReferencedEnvelope;
-import org.n52.wps.io.data.IData;
-import org.n52.wps.io.data.binding.complex.GTVectorDataBinding;
-import org.n52.wps.io.data.binding.literal.LiteralAnyURIBinding;
-import org.n52.wps.io.data.binding.literal.LiteralDateTimeBinding;
-import org.n52.wps.io.data.binding.literal.LiteralStringBinding;
 import ucar.ma2.InvalidRangeException;
 import ucar.ma2.Range;
 import ucar.nc2.constants.FeatureType;
@@ -29,103 +20,14 @@ import ucar.nc2.ft.FeatureDatasetFactoryManager;
  *
  * @author tkunicki
  */
-public abstract class GDPAlgorithmUtil extends AbstractSelfDescribingAlgorithm {
+public abstract class GDPAlgorithmUtil {
 
     public final static String INPUT_FEATURE_COLLECTION = "FEATURE_COLLECTION";
     public final static String INPUT_FEATURE_ATTRIBUTE_NAME = "FEATURE_ATTRIBUTE_NAME";
     public final static String INPUT_DATASET_URI = "DATASET_URI";
     public final static String INPUT_DATASET_ID = "DATASET_ID";
 
-    public GDPAlgorithmUtil() {
-            super();
-    }
-	
-    public static FeatureCollection extractFeatureCollection(Map<String, List<IData>> input, String id) {
-            GTVectorDataBinding vectorDataBinding = null;
-            List<IData> iDataList = input.get(id);
-            if (iDataList != null && iDataList.size() == 1) {
-                    IData iData = iDataList.get(0);
-                    if (iData instanceof GTVectorDataBinding) {
-                            vectorDataBinding = (GTVectorDataBinding) iData;
-                    }
-            }
-            return vectorDataBinding.getPayload();
-    }
-
-	public static List<String> extractStringList(Map<String, List<IData>> input, String id) {
-		List<String> stringList = new ArrayList<String>();
-		List<IData> iDataList = input.get(id);
-		if (iDataList != null) {
-			for(IData data : iDataList) {
-				if (data instanceof LiteralStringBinding) {
-					stringList.add(((LiteralStringBinding)data).getPayload());
-				}
-			}
-
-		}
-		return stringList;
-	}
-
-    public static List<Date> extractDateList(Map<String, List<IData>> input, String id) {
-		List<Date> dateList = new ArrayList<Date>();
-		List<IData> iDataList = input.get(id);
-		if (iDataList != null) {
-			for(IData data : iDataList) {
-				if (data instanceof LiteralDateTimeBinding) {
-					dateList.add(((LiteralDateTimeBinding)data).getPayload());
-				}
-			}
-
-		}
-		return dateList;
-	}
-
-    public static List<URI> extractURIList(Map<String, List<IData>> input, String id) {
-		List<URI> dateList = new ArrayList<URI>();
-		List<IData> iDataList = input.get(id);
-		if (iDataList != null) {
-			for(IData data : iDataList) {
-				if (data instanceof LiteralAnyURIBinding) {
-					dateList.add(((LiteralAnyURIBinding)data).getPayload());
-				}
-			}
-
-		}
-		return dateList;
-	}
-
-    public static String extractString(Map<String, List<IData>> input, String id) {
-		List<String> stringList = extractStringList(input, id);
-        if (stringList == null || stringList.size() < 1) {
-            return null;
-        }
-        if (stringList.size() == 1) {
-            return  stringList.get(0);
-        }
-		throw new RuntimeException("too many arguments for input id " + id);
-	}
-
-    public static Date extractDate(Map<String, List<IData>> input, String id) {
-		List<Date> dateList = extractDateList(input, id);
-        if (dateList == null || dateList.size() < 1) {
-            return null;
-        }
-        if (dateList.size() == 1) {
-            return  dateList.get(0);
-        }
-		throw new RuntimeException("too many arguments for input id " + id);
-	}
-
-    public static URI extractURI(Map<String, List<IData>> input, String id) {
-		List<URI> uriList = extractURIList(input, id);
-        if (uriList == null || uriList.size() < 1) {
-            return null;
-        }
-        if (uriList.size() == 1) {
-            return  uriList.get(0);
-        }
-		throw new RuntimeException("too many arguments for input id " + id);
-	}
+    private GDPAlgorithmUtil() { }
 
     public static GridDatatype generateGridDataType(URI datasetURI, String datasetId, ReferencedEnvelope featureBounds) {
         GridDatatype gridDatatype = null;
