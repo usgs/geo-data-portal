bc955f36a87ddd2456b12c87b4fe6f2fcfad1be1
tkunicki <tkunicki@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-04-14 21:33:16 +0000

    Added logging to RetrieveResultServlet Renamed parameter "asattachment" to "attachment" Cleaned up "attachment" parameter semantics.  If missing defaults to true for every mime-type except those for xml.
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@14349 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- gdp-process-wps/src/main/java/org/n52/wps/server/RetrieveResultServlet.java
+++ gdp-process-wps/src/main/java/org/n52/wps/server/RetrieveResultServlet.java
@@ -13,23 +13,31 @@ import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.n52.wps.server.database.DatabaseFactory;
 import org.n52.wps.server.database.IDatabase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RetrieveResultServlet extends HttpServlet {
 
-	private static final long serialVersionUID = -268198171054599696L;
-
-	public static String SERVLET_PATH = "RetrieveResultServlet";
+    private final static Logger LOGGER = LoggerFactory.getLogger(RetrieveResultServlet.class);
+    
+    private static final long serialVersionUID = -268198171054599696L;
+    
+    // This is required for URL generation for response documents.
+    public final static String SERVLET_PATH = "RetrieveResultServlet";
 
     @Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		String id = request.getParameter("id");
-                String asAttachmentParam = request.getParameter("asattachment");
-                boolean asAttachment = (StringUtils.isNotBlank(asAttachmentParam) || Boolean.parseBoolean(asAttachmentParam)) ? true : false;
-                
-		if(id == null || id.length() == 0) {
-			errorResponse("id parameter missing", response);
-		} else {
+        // id of result to retrieve.
+        String id = request.getParameter("id");
+        
+        // return result as attachment (instructs browser to offer user "Save" dialog
+        // if document is XML the default is false, otherwise the default is true.
+        String attachment = request.getParameter("attachment");
+
+        if (StringUtils.isEmpty(id)) {
+            errorResponse("id parameter missing", response);
+        } else {
             IDatabase db = DatabaseFactory.getDatabase();
             String mimeType = db.getMimeTypeForStoreResponse(id);
             InputStream is = db.lookupResponse(id);
@@ -37,13 +45,19 @@ public class RetrieveResultServlet extends HttpServlet {
                 if (mimeType == null || is == null) {
                     errorResponse("id parameter invalid", response);
                 } else {
-                    String suffix = MIMEUtil.getSuffixFromMIMEType(mimeType);
-                    if (!"xml".equals(suffix) || asAttachment) {
+                    String suffix = MIMEUtil.getSuffixFromMIMEType(mimeType).toLowerCase();
+                    boolean useAttachment = (attachment.isEmpty() && !"xml".equals(suffix)) || Boolean.parseBoolean(attachment);
+                    if (useAttachment) {
                         response.addHeader("Content-Disposition", "attachment; filename=\"wps-result." + suffix + "\"");
                     }
+                    // set Content-Type before getting outputstream, this allows
+                    // for output stream 
                     response.setContentType(mimeType);
                     OutputStream os = response.getOutputStream();
-                    IOUtils.copy(is, os);
+                    long bytes = IOUtils.copyLarge(is, os);
+                    if (bytes <= 0) {
+                        LOGGER.warn("Empty response returned for id {}", id);
+                    }
                     os.flush();
                 }
             } finally {
@@ -51,13 +65,13 @@ public class RetrieveResultServlet extends HttpServlet {
             }
         }
         response.flushBuffer();
-	}
+    }
 
     protected void errorResponse(String error, HttpServletResponse response) throws IOException {
-			response.setContentType("text/html");
-			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-			PrintWriter writer = response.getWriter();
-			writer.write("<html><title>Error</title><body>" + error + "</body></html>");
-            writer.flush();
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        PrintWriter writer = response.getWriter();
+        writer.write("<html><title>Error</title><body>" + error + "</body></html>");
+        writer.flush();
     }
 }
