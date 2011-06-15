2aedeafcf4389cefd0bbb5cf210de0efc3e13c21
razoerb <razoerb@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-06-15 14:42:51 +0000

    Add GeoServer extension for setting bbox of features added over WFS-T.
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@15424 622dc1fb-2224-0410-ab72-dc85a3975e1a

---
+++ transaction-bbox/src/main/java/org/geoserver/transactionbbox/BboxTransactionListener.java
@@ -0,0 +1,42 @@
+/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org. All rights reserved.
+ * This code is licensed under the GPL 2.0 license, availible at the root
+ * application directory.
+ */
+package org.geoserver.transactionbbox;
+
+import org.geoserver.catalog.Catalog;
+import org.geoserver.catalog.LayerInfo;
+import org.geoserver.catalog.ResourceInfo;
+import org.geoserver.wfs.*;
+import org.geotools.feature.NameImpl;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+
+
+public class BboxTransactionListener implements TransactionListener {
+    
+    Catalog catalog;
+
+    public BboxTransactionListener(Catalog catalog) {
+        this.catalog = catalog;
+    }
+    
+    /**
+     * Check/alter feature collections and filters before a change hits the
+     * datastores
+     */
+    public void dataStoreChange(TransactionEvent event) throws WFSException {
+
+        if (event.getType() == TransactionEventType.PRE_INSERT) {
+
+            LayerInfo layerInfo = catalog.getLayerByName(new NameImpl(event.getLayerName()));
+            ResourceInfo resourceInfo = layerInfo.getResource();
+
+            ReferencedEnvelope bounds = event.getAffectedFeatures().getBounds();
+
+            resourceInfo.setLatLonBoundingBox(bounds);
+            resourceInfo.setNativeBoundingBox(bounds);
+
+            catalog.save(resourceInfo);
+        }
+    }
+}
