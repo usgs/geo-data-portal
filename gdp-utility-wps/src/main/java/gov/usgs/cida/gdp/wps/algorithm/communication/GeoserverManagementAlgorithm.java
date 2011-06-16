c9752cd4a4902ab78be9bfc60fdbf5202bb6f645
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-06-16 19:58:10 +0000

    bracketized ifzor statementsz
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@15491 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- gdp-utility-wps/src/main/java/gov/usgs/cida/gdp/wps/algorithm/communication/GeoserverManagementAlgorithm.java
+++ gdp-utility-wps/src/main/java/gov/usgs/cida/gdp/wps/algorithm/communication/GeoserverManagementAlgorithm.java
@@ -20,12 +20,24 @@ import org.n52.wps.server.AbstractSelfDescribingAlgorithm;
 public class GeoserverManagementAlgorithm extends AbstractSelfDescribingAlgorithm  {
     @Override
     public Map<String, IData> run(Map<String, List<IData>> inputData) {
-        if (inputData == null) throw new RuntimeException("Error while allocating input parameters.");
-        if (!inputData.containsKey("command")) throw new RuntimeException("Error: Missing input parameter 'command'");
-        if (!inputData.containsKey("username")) throw new RuntimeException("Error: Missing input parameter 'username'");
-        if (!inputData.containsKey("password")) throw new RuntimeException("Error: Missing input parameter 'password'");
-        if (!inputData.containsKey("wfsHost")) throw new RuntimeException("Error: Missing input parameter 'wfsHost'");
-        if (!inputData.containsKey("wfsPort")) throw new RuntimeException("Error: Missing input parameter 'wfsPort'");
+        if (inputData == null) {
+            throw new RuntimeException("Error while allocating input parameters.");
+        }
+        if (!inputData.containsKey("command")) {
+            throw new RuntimeException("Error: Missing input parameter 'command'");
+        }
+        if (!inputData.containsKey("username")) {
+            throw new RuntimeException("Error: Missing input parameter 'username'");
+        }
+        if (!inputData.containsKey("password")) {
+            throw new RuntimeException("Error: Missing input parameter 'password'");
+        }
+        if (!inputData.containsKey("wfsHost")) {
+            throw new RuntimeException("Error: Missing input parameter 'wfsHost'");
+        }
+        if (!inputData.containsKey("wfsPort")) {
+            throw new RuntimeException("Error: Missing input parameter 'wfsPort'");
+        }
 
         String command, username, password, workspace, wfsHost, wfsPort = null;
         List<String> datastoreList = new ArrayList<String>();
@@ -33,27 +45,37 @@ public class GeoserverManagementAlgorithm extends AbstractSelfDescribingAlgorith
         // Pull in our command
         List<IData> dataList = inputData.get("command");
         command = ((LiteralStringBinding)dataList.get(0)).getPayload();
-        if (StringUtils.isBlank(command)) throw new RuntimeException("Error: Missing input parameter 'command'");
+        if (StringUtils.isBlank(command)) {
+            throw new RuntimeException("Error: Missing input parameter 'command'");
+        }
 
         // Pull in our host
         dataList = inputData.get("wfsHost");
         wfsHost = ((LiteralStringBinding)dataList.get(0)).getPayload();
-        if (StringUtils.isBlank(wfsHost)) throw new RuntimeException("Error: Missing input parameter 'wfsHost'");
+        if (StringUtils.isBlank(wfsHost)) {
+            throw new RuntimeException("Error: Missing input parameter 'wfsHost'");
+        }
 
         // Pull in our host
         dataList = inputData.get("wfsPort");
         wfsPort = ((LiteralStringBinding)dataList.get(0)).getPayload();
-        if (StringUtils.isBlank(wfsPort)) throw new RuntimeException("Error: Missing input parameter 'wfsPort'");
+        if (StringUtils.isBlank(wfsPort)) {
+            throw new RuntimeException("Error: Missing input parameter 'wfsPort'");
+        }
 
         // Pull in our username
         dataList = inputData.get("username");
         username = ((LiteralStringBinding)dataList.get(0)).getPayload();
-        if (StringUtils.isBlank(username)) throw new RuntimeException("Error: Missing input parameter 'username'");
+        if (StringUtils.isBlank(username)) {
+            throw new RuntimeException("Error: Missing input parameter 'username'");
+        }
 
         // Pull in our password
         dataList = inputData.get("password");
         password = ((LiteralStringBinding)dataList.get(0)).getPayload();
-        if (StringUtils.isBlank(password)) throw new RuntimeException("Error: Missing input parameter 'password'");
+        if (StringUtils.isBlank(password)) {
+            throw new RuntimeException("Error: Missing input parameter 'password'");
+        }
 
         // Pull in our workspace, if any
         dataList = inputData.get("workspace");
@@ -64,8 +86,12 @@ public class GeoserverManagementAlgorithm extends AbstractSelfDescribingAlgorith
         for (IData iData : dataList) datastoreList.add(((LiteralStringBinding)iData).getPayload());
 
         if ("delete".equalsIgnoreCase(command)) {
-            if (datastoreList.isEmpty()) throw new RuntimeException("Error: 'delete' called but no datastore was provided");
-            if (StringUtils.isBlank(workspace)) throw new RuntimeException("Error: 'delete' called but no workspace was provided");
+            if (datastoreList.isEmpty()) {
+                throw new RuntimeException("Error: 'delete' called but no datastore was provided");
+            }
+            if (StringUtils.isBlank(workspace)) {
+                throw new RuntimeException("Error: 'delete' called but no workspace was provided");
+            }
             
             GeoserverManager gm = new GeoserverManager(
                     "http://" + wfsHost + ":" + wfsPort + "/geoserver", username, password);
@@ -77,8 +103,12 @@ public class GeoserverManagementAlgorithm extends AbstractSelfDescribingAlgorith
         } else throw new RuntimeException("Error: Unrecognized command: " + command);
 
         Map<String, IData> result = new HashMap<String, IData>();
-        if (getErrors().isEmpty()) result.put("result", new LiteralStringBinding("Your request has completed successfully."));
-        else result.put("result", new LiteralStringBinding("Your request has completed with errors."));
+        if (getErrors().isEmpty()) {
+            result.put("result", new LiteralStringBinding("Your request has completed successfully."));
+        }
+        else {
+            result.put("result", new LiteralStringBinding("Your request has completed with errors."));
+        }
 
         return result;
     }
@@ -98,25 +128,39 @@ public class GeoserverManagementAlgorithm extends AbstractSelfDescribingAlgorith
 
     @Override
     public BigInteger getMaxOccurs(String identifier) {
-        if ("command".equals(identifier)) return BigInteger.valueOf(1);
-        if ("wfsHost".equals(identifier)) return BigInteger.valueOf(1);
-        if ("wfsPort".equals(identifier)) return BigInteger.valueOf(1);
-        if ("username".equals(identifier)) return BigInteger.valueOf(1);
-        if ("password".equals(identifier)) return BigInteger.valueOf(1);
-        if ("workspace".equals(identifier)) return BigInteger.valueOf(1);
-        if ("datastore".equals(identifier)) return BigInteger.valueOf(1);
+        if ("command".equals(identifier)) { return BigInteger.valueOf(1); }
+        if ("wfsHost".equals(identifier)) { return BigInteger.valueOf(1); }
+        if ("wfsPort".equals(identifier)) { return BigInteger.valueOf(1); }
+        if ("username".equals(identifier)) { return BigInteger.valueOf(1); }
+        if ("password".equals(identifier)) { return BigInteger.valueOf(1); }
+        if ("workspace".equals(identifier)) { return BigInteger.valueOf(1); }
+        if ("datastore".equals(identifier)) { return BigInteger.valueOf(1); }
         return super.getMaxOccurs(identifier);
     }
 
     @Override
     public BigInteger getMinOccurs(String identifier) {
-        if ("command".equals(identifier)) return BigInteger.valueOf(1);
-        if ("wfsHost".equals(identifier)) return BigInteger.valueOf(1);
-        if ("wfsPort".equals(identifier)) return BigInteger.valueOf(1);
-        if ("username".equals(identifier)) return BigInteger.valueOf(1);
-        if ("password".equals(identifier)) return BigInteger.valueOf(1);
-        if ("workspace".equals(identifier)) return BigInteger.valueOf(0);
-        if ("datastore".equals(identifier)) return BigInteger.valueOf(0);
+        if ("command".equals(identifier)) {
+            return BigInteger.valueOf(1);
+        }
+        if ("wfsHost".equals(identifier)) {
+            return BigInteger.valueOf(1);
+        }
+        if ("wfsPort".equals(identifier)) {
+            return BigInteger.valueOf(1);
+        }
+        if ("username".equals(identifier)) {
+            return BigInteger.valueOf(1);
+        }
+        if ("password".equals(identifier)) {
+            return BigInteger.valueOf(1);
+        }
+        if ("workspace".equals(identifier)) {
+            return BigInteger.valueOf(0);
+        }
+        if ("datastore".equals(identifier)) {
+            return BigInteger.valueOf(0);
+        }
         return super.getMaxOccurs(identifier);
     }
 
@@ -127,21 +171,42 @@ public class GeoserverManagementAlgorithm extends AbstractSelfDescribingAlgorith
         return result;
     }
 
+    /**
+     * 
+     * @param id
+     * @return
+     */
     @Override
     public Class getInputDataType(String id) {
-        if ("command".equals(id)) return LiteralStringBinding.class;
-        if ("wfsHost".equals(id)) return LiteralStringBinding.class;
-        if ("wfsPort".equals(id)) return LiteralStringBinding.class;
-        if ("username".equals(id)) return LiteralStringBinding.class;
-        if ("password".equals(id)) return LiteralStringBinding.class;
-        if ("workspace".equals(id)) return LiteralStringBinding.class;
-        if ("datastore".equals(id)) return LiteralStringBinding.class;
+        if ("command".equals(id)) {
+            return LiteralStringBinding.class;
+        }
+        if ("wfsHost".equals(id)) {
+            return LiteralStringBinding.class;
+        }
+        if ("wfsPort".equals(id)) {
+            return LiteralStringBinding.class;
+        }
+        if ("username".equals(id)) {
+            return LiteralStringBinding.class;
+        }
+        if ("password".equals(id)) {
+            return LiteralStringBinding.class;
+        }
+        if ("workspace".equals(id)) {
+            return LiteralStringBinding.class;
+        }
+        if ("datastore".equals(id)) {
+            return LiteralStringBinding.class;
+        }
         return null;
     }
 
     @Override
     public Class getOutputDataType(String id) {
-        if ("result".equals(id)) return LiteralStringBinding.class;
+        if ("result".equals(id)) {
+            return LiteralStringBinding.class;
+        }
         return null;
     }
 
