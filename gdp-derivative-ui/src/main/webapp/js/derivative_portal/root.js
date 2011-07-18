6efcd91619bf9b77748186356f3841d857f38024
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-07-18 19:11:48 +0000

    Dataset now being shown over base layer
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@16256 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- gdp-derivative-ui/src/main/webapp/js/derivative_portal/root.js
+++ gdp-derivative-ui/src/main/webapp/js/derivative_portal/root.js
@@ -14,12 +14,11 @@ Ext.onReady(function () {
     initializeLogging();
         
     var map = new OpenLayers.Map();
-    var layer = new OpenLayers.Layer.WMS(
+    var baseLayer = new OpenLayers.Layer.WMS(
         "Global Imagery",
         "http://isse.cr.usgs.gov/ArcGIS/services/Combined/SDDS_Imagery/MapServer/WMSServer",
                
         {
-            
             layers: "0"
         }
         );
@@ -27,13 +26,18 @@ Ext.onReady(function () {
         "...",
         "http://igsarm-cida-thredds1.er.usgs.gov:8080/thredds/wms/gmo/GMO_w_meta.ncml",
         {
+            transparent: 'true',
             isBaseLayer: false,
             layers: "Prcp"
+        },{
+            opacity : 0.5
         }
         );
             
-    map.addLayers([layer, threddsLayer]);
-    map.raiseLayer(threddsLayer, 1);
+    map.addLayers([baseLayer, threddsLayer]);
+    map.setBaseLayer(baseLayer);
+    map.setLayerIndex(threddsLayer, 1);
+//    map.raiseLayer(threddsLayer, 1);
 //    map.addLayers([threddsLayer,layer]);
 //    map.addLayer(threddsLayer);
 
