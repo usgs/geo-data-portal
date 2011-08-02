17661f90bf1b01c2b65a8ab30a47722074ee286e
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-08-02 19:59:10 +0000

    Removed dangling comma
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@16516 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- gdp-derivative-ui/src/main/webapp/js/derivative_portal/root.js
+++ gdp-derivative-ui/src/main/webapp/js/derivative_portal/root.js
@@ -239,7 +239,7 @@ function initializeMapping() {
 		layout : 'fit',
 		border: false,
 		layerController : layerController,
-		title: 'USGS Derived Downscaled Climate Portal',
+		title: 'USGS Derived Downscaled Climate Portal'
 	});
 	
 	var centerPanel = new Ext.Panel({
