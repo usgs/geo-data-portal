b1f13a7fb5dbc3ef720180c6f6283a3afbab77a5
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-08-04 11:03:34 +0000

    Added more navigation controls to the map activity bar
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@16577 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- gdp-derivative-ui/src/main/webapp/js/derivative_portal/components/MapActivityBar.js
+++ gdp-derivative-ui/src/main/webapp/js/derivative_portal/components/MapActivityBar.js
@@ -10,9 +10,37 @@ GDP.MapActivityBar = Ext.extend(Ext.Toolbar, {
         this.layerController = config.layerController;
         
         var zoomToExtentAction = new GeoExt.Action({
-            text : 'Max Extent'
-        })
+            text : 'Max Extent',
+            control: new OpenLayers.Control.ZoomToMaxExtent(),
+            tooltip: "zoom to max extent"
+        });
         
+        var navigationAction = new GeoExt.Action({
+            text: "Nav",
+            control: new OpenLayers.Control.Navigation(),
+            toggleGroup: "draw",
+            allowDepress: false,
+            pressed: true,
+            tooltip: "navigate",
+            group: "draw",
+            checked: true
+        });
+        
+        var vector = new OpenLayers.Layer.Vector("vector");
+        var drawPolygonAction = new GeoExt.Action({
+        text: "draw poly",
+        control: new OpenLayers.Control.DrawFeature(
+            vector, OpenLayers.Handler.Polygon
+        ),
+        toggleGroup: "draw",
+        allowDepress: false,
+        tooltip: "draw polygon",
+        group: "draw"
+    });
+        
+        config = Ext.apply({
+            items: [zoomToExtentAction, navigationAction]
+        }, config);
         GDP.MapActivityBar.superclass.constructor.call(this, config);
     }
 });
