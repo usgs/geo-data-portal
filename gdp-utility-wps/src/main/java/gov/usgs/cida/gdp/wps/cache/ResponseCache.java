98cbae28358ed5ba5bf917c277ec6de3cb100d75
jiwalker <jiwalker@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-08-04 22:38:48 +0000

    created class to manage the response cache, contains a request type enum and bean for using it
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@16629 622dc1fb-2224-0410-ab72-dc85a3975e1a

---
+++ gdp-utility-wps/src/main/java/gov/usgs/cida/gdp/wps/cache/ResponseCache.java
@@ -0,0 +1,92 @@
+package gov.usgs.cida.gdp.wps.cache;
+
+import gov.usgs.cida.gdp.constants.AppConstant;
+import gov.usgs.cida.gdp.utilities.bean.XmlResponse;
+import gov.usgs.cida.gdp.wps.cache.ResponseCache.CacheIdentifier.CacheType;
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Jordan Walker <jiwalker@usgs.gov>
+ */
+public class ResponseCache {
+
+    private static final Logger log = LoggerFactory.getLogger(
+            ResponseCache.class);
+
+    public static synchronized XmlResponse readXmlFromCache(CacheIdentifier ci) {
+        File cacheFile = ci.getFile();
+        log.debug("Reading from {} cache for {} at file location {}",
+                  new Object[] { ci.cacheType.getType(), ci.datasetUri, cacheFile });
+        RawXmlResponse xml = new RawXmlResponse(cacheFile);
+        return xml;
+    }
+
+    public static synchronized boolean hasCachedResponse(CacheIdentifier ci) {
+        File cacheFile = ci.getFile();
+        return cacheFile.exists();
+    }
+
+    public static synchronized void writeXmlToCache(CacheIdentifier ci,
+                                                    XmlResponse xml) throws
+            IOException {
+        File cacheFile = ci.getFile();
+        log.debug("Writing to {} cache for {} at file location {}",
+                  new Object[] { ci.cacheType.getType(), ci.datasetUri, cacheFile });
+        FileUtils.write(cacheFile, xml.toXML());
+    }
+
+    public static class CacheIdentifier {
+
+        public enum CacheType {
+
+            DATA_TYPE,
+            TIME_RANGE;
+
+            public String getCacheDir() {
+                String cacheDir = AppConstant.CACHE_LOCATION.getValue();
+                switch (this) {
+                    case DATA_TYPE:
+                        return cacheDir + File.separator + "data_types";
+                    case TIME_RANGE:
+                        return cacheDir + File.separator + "time_ranges";
+                    default:
+                        return cacheDir;
+                }
+            }
+
+            public String getType() {
+                switch (this) {
+                    case DATA_TYPE:
+                        return "data type";
+                    case TIME_RANGE:
+                        return "time range";
+                    default:
+                        return "";
+                }
+            }
+        }
+        
+        public String datasetUri;
+        public CacheType cacheType;
+        public String grid;
+        
+        public CacheIdentifier(String datasetUri, CacheType cacheType, String grid) {
+            this.datasetUri = datasetUri;
+            this.cacheType = cacheType;
+            this.grid = grid;
+        }
+        
+        public File getFile() {
+            String replaced = datasetUri.replaceAll("[\\:/]", "-");
+            if (grid != null && !grid.isEmpty()) {
+                replaced += "-" + grid;
+            }
+            return new File(cacheType.getCacheDir() + File.separator + replaced + ".cache");
+        }
+    }
+}
