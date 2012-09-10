3de986181845924a2b12b0443b65da6e51e11a67
tkunicki <tkunicki@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2012-09-10 18:56:32 +0000

    52n WPS 3.0 refactor: updated XMLOptions for DescribeProcess output
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@25600 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- gdp-52n-wps-patches/src/main/java/org/n52/wps/server/response/DescribeProcessResponse.java
+++ gdp-52n-wps-patches/src/main/java/org/n52/wps/server/response/DescribeProcessResponse.java
@@ -53,13 +53,12 @@ public class DescribeProcessResponse extends Response{
 	public InputStream getAsStream() throws ExceptionReport{
 		try {
 			XmlOptions opts = new XmlOptions();
-			opts.setSaveNamespacesFirst();
-//            HashMap ns = new HashMap();
-//            ns.put("http://www.opengis.net/wps/1.0.0", "wps");
-//            ns.put("http://www.opengis.net/ows/1.1", "ows");
-//            opts.setSaveNamespacesFirst().
-//                setSaveSuggestedPrefixes(ns).
-//                setSaveAggressiveNamespaces();
+            HashMap ns = new HashMap();
+            ns.put("http://www.opengis.net/wps/1.0.0", "wps");
+            ns.put("http://www.opengis.net/ows/1.1", "ows");
+            opts.setSaveNamespacesFirst().
+                setSaveSuggestedPrefixes(ns).
+                setSaveAggressiveNamespaces();
 			return ((ProcessDescriptionsDocument)request.getAttachedResult()).newInputStream(opts);
 		}
 		catch(Exception e) {
