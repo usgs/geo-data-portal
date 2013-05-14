65c83a72c2595c4d215ac021f0328318efdabaec
tkunicki <tkunicki@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2013-05-14 08:26:32 +0000

    Modified binding for FeatureCollection to facilitate cleanup (part 2)
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/gdp/trunk@32673 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- gdp-process-wps/src/main/java/gov/usgs/cida/gdp/wps/parser/GMLStreamingParser.java
+++ gdp-process-wps/src/main/java/gov/usgs/cida/gdp/wps/parser/GMLStreamingParser.java
@@ -1,24 +1,24 @@
 package gov.usgs.cida.gdp.wps.parser;
 
+import gov.usgs.cida.gdp.wps.binding.GMLStreamingFeatureCollectionBinding;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import org.apache.commons.io.FileUtils;
-import org.n52.wps.io.data.binding.complex.GTVectorDataBinding;
 import org.n52.wps.io.datahandler.parser.AbstractParser;
 
 public class GMLStreamingParser extends AbstractParser {
 
 	public GMLStreamingParser() {
-		supportedIDataTypes.add(GTVectorDataBinding.class);
+		supportedIDataTypes.add(GMLStreamingFeatureCollectionBinding.class);
 	}
 
     @Override
-    public GTVectorDataBinding parse(InputStream input, String mimeType, String schema) {
+    public GMLStreamingFeatureCollectionBinding parse(InputStream input, String mimeType, String schema) {
         try {
             File tempFile = File.createTempFile(getClass().getSimpleName(), ".xml");
             FileUtils.copyInputStreamToFile(input, tempFile);
-			return new GTVectorDataBinding(new GMLStreamingFeatureCollection(tempFile));
+			return new GMLStreamingFeatureCollectionBinding(new GMLStreamingFeatureCollection(tempFile));
 		} catch (IOException e) {
 			throw new RuntimeException("Error creating temporary file", e);
 		}
