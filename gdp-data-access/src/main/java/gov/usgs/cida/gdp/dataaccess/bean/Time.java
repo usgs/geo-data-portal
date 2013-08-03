8ae27b8485bdb1d4d7f85b3afb54f88858e109fe
Ivan Suftin <isuftin@usgs.gov>
2013-08-03 18:35:06 -0500

    Updated to not cache empty results (when no grids exist for ListOpendapGrids and when no times exist for GetGridTimeRange Issue #GDP-531 - Utility WPS date handling

--- gdp-data-access/src/main/java/gov/usgs/cida/gdp/dataaccess/bean/Time.java
+++ gdp-data-access/src/main/java/gov/usgs/cida/gdp/dataaccess/bean/Time.java
@@ -17,8 +17,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.LoggerFactory;
 
